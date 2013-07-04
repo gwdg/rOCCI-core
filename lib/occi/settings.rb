1ba7e21e248f072cca7220ec7c032baa5ed63f20
Boris Parak <256254@mail.muni.cz>
2013-07-04 23:23:51 +0200

    Small fixes
    * Fixed gemspec (split, activesupport version)
    * Fixed Settings (broken path in gem form)

--- lib/occi/settings.rb
+++ lib/occi/settings.rb
@@ -1,6 +1,8 @@
 module Occi
   class Settings < Settingslogic
-    source 'config/occi.yml'
+    gem_root = File.expand_path '../../..', __FILE__
+
+    source "#{gem_root}/config/occi.yml"
     source open(ENV['HOME']+'/.occi') if File.file?(ENV['HOME']+'/.occi')
     namespace 'core'
   end
