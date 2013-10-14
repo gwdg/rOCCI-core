aa1d820761a01ae5329609c72ba7e7f776f1bbfe
Zdenek Sustr <git@sustr.net>
2013-10-14 16:51:21 +0200

    TABs replaced with spaces

--- spec/occi/core/kind_spec.rb
+++ spec/occi/core/kind_spec.rb
@@ -11,10 +11,10 @@ module Occi
 
           it 'by term' do
             expect(klass).to be Occi::Core::Resource
-	  end
+          end
           it 'by scheme' do
             expect(klass.superclass).to be Occi::Core::Entity
-	  end
+          end
         end
 
         context 'gets non predefined OCCI class by term, scheme and related class' do
@@ -25,10 +25,10 @@ module Occi
 
           it 'by term' do
             expect(klass).to be Com::Example::Occi::Test
-	  end
+          end
           it 'by scheme' do
             expect(klass.superclass).to be Occi::Core::Resource
-	  end
+          end
         end
 
         it 'does not get OCCI class by term and scheme if it relates to existing class not derived from OCCI Entity' do
@@ -47,40 +47,40 @@ module Occi
 
         it 'recognizes existing relationship' do
           expect(related.related_to?(base)).to eq true
-	end
+        end
 
         it 'does not give false positives on non-existent relationship' do
           expect(base.related_to?(unrelated)).to eq false
         end
 
         it 'recognizes transitive relationships' #do #TODO This test actually works, but fails because te feature is not yet implemented
-#	  grandchild = Occi::Core::Kind.new 'http://occi.test.case/core/kind/base', 'related', 'title', Occi::Core::Attributes.new, related
+#          grandchild = Occi::Core::Kind.new 'http://occi.test.case/core/kind/base', 'related', 'title', Occi::Core::Attributes.new, related
 #          expect(grandchild.related_to?(base)).to eq true
-#	end
+#        end
       end
 
       describe '#as_json' do
         kind = Occi::Core::Kind.new
 
         it 'renders JSON correctly from freshly initialized object' do
-	  expected = '{"location":"/kind/","term":"kind","scheme":"http://schemas.ogf.org/occi/core#"}'
-	  hash=Hashie::Mash.new(JSON.parse(expected))
-	  expect(kind.as_json).to eql(hash)
+          expected = '{"location":"/kind/","term":"kind","scheme":"http://schemas.ogf.org/occi/core#"}'
+          hash=Hashie::Mash.new(JSON.parse(expected))
+          expect(kind.as_json).to eql(hash)
         end
 
         it 'renders JSON correctly with optional attributes' do
-	  kind.title = "test title"
-	  expected = '{"location":"/kind/","term":"kind","scheme":"http://schemas.ogf.org/occi/core#","title":"test title"}'
-	  hash=Hashie::Mash.new(JSON.parse(expected))
-	  expect(kind.as_json).to eql(hash)
+          kind.title = "test title"
+          expected = '{"location":"/kind/","term":"kind","scheme":"http://schemas.ogf.org/occi/core#","title":"test title"}'
+          hash=Hashie::Mash.new(JSON.parse(expected))
+          expect(kind.as_json).to eql(hash)
         end
 
 
         it 'renders JSON correctly with special characters' do
-	  kind.title = "Some special characters @#\$%"
-	  expected = '{"location":"/kind/","term":"kind","scheme":"http://schemas.ogf.org/occi/core#","title":"Some special characters @#\$%"}'
-	  hash=Hashie::Mash.new(JSON.parse(expected))
-	  expect(kind.as_json).to eql(hash)
+          kind.title = "Some special characters @#\$%"
+          expected = '{"location":"/kind/","term":"kind","scheme":"http://schemas.ogf.org/occi/core#","title":"Some special characters @#\$%"}'
+          hash=Hashie::Mash.new(JSON.parse(expected))
+          expect(kind.as_json).to eql(hash)
         end
 
       end
@@ -89,19 +89,19 @@ module Occi
         kind = Occi::Core::Kind.new
 
         it 'produces a string correctly from freshly initialized object' do
-	  expected = ('scheme="http://schemas.ogf.org/occi/core#";class="kind";location="/kind/";kind').split(/;/)
-	  actual = kind.to_string.split(/;/)
-	  expect(actual).to match_array(expected)
+          expected = ('scheme="http://schemas.ogf.org/occi/core#";class="kind";location="/kind/";kind').split(/;/)
+          actual = kind.to_string.split(/;/)
+          expect(actual).to match_array(expected)
         end
 
         it 'produces a string correctly with optional attributes' do
-	  kind.title = "test title"
-	  expected = ('scheme="http://schemas.ogf.org/occi/core#";class="kind";location="/kind/";kind;title="test title"').split(/;/)
-	  actual = kind.to_string.split(/;/)
-	  expect(actual).to match_array(expected)
+          kind.title = "test title"
+          expected = ('scheme="http://schemas.ogf.org/occi/core#";class="kind";location="/kind/";kind;title="test title"').split(/;/)
+          actual = kind.to_string.split(/;/)
+          expect(actual).to match_array(expected)
         end
 
-#	TODO: Optional attributes, special characters
+#        TODO: Optional attributes, special characters
 
       end
 
