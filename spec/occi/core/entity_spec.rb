452d7c1da0add6b664395754d208ca982c9cd195
Zdenek Sustr <sustr4@cesnet.cz>
2013-10-18 14:50:31 +0200

    Basic action tests plus slight reorganization

--- spec/occi/core/entity_spec.rb
+++ spec/occi/core/entity_spec.rb
@@ -1,50 +1,49 @@
 module Occi
   module Core
     describe Entity do
+      let(:entity){ Occi::Core::Entity.new }
+      let(:testaction){ Occi::Core::Action.new scheme='http://schemas.ogf.org/occi/core/entity/action#', term='testaction', title='testaction action' }
 
       it "initializes itself successfully" do
-        entity = Occi::Core::Entity.new
         expect(entity).to be_kind_of Occi::Core::Entity
       end
 
       context 'initializiation of a subclass using a type identifier' do
         let(:type_identifier){ 'http://example.com/testnamespace#test' }
-        let(:entity){ Occi::Core::Entity.new type_identifier }
+        let(:typed_entity){ Occi::Core::Entity.new type_identifier }
         it 'has the correct kind' do
-          expect(entity).to be_kind_of 'Com::Example::Testnamespace::Test'.constantize
+          expect(typed_entity).to be_kind_of 'Com::Example::Testnamespace::Test'.constantize
         end
         it 'uses the right identifier' do
-          expect(entity.kind.type_identifier).to eq type_identifier
+          expect(typed_entity.kind.type_identifier).to eq type_identifier
         end
         it 'relates to Entity' do
-          expect(entity.kind.related.first).to eq Occi::Core::Entity.kind
+          expect(typed_entity.kind.related.first).to eq Occi::Core::Entity.kind
         end
       end
 
       context "initialization of a subclass using an OCCI Kind" do
         let(:kind){ Occi::Core::Resource.kind }
-        let(:entity){ Occi::Core::Entity.new kind }
+        let(:kind_entity){ Occi::Core::Entity.new kind }
         it 'has the correct kind' do
-          expect(entity).to be_kind_of Occi::Core::Resource
+          expect(kind_entity).to be_kind_of Occi::Core::Resource
         end
         it 'uses the right identifier' do
-          expect(entity.kind.type_identifier).to eq Occi::Core::Resource.type_identifier
+          expect(kind_entity.kind.type_identifier).to eq Occi::Core::Resource.type_identifier
         end
         it 'relates to Entity' do
-          expect(entity.kind.related.first).to eq Occi::Core::Entity.kind
+          expect(kind_entity.kind.related.first).to eq Occi::Core::Entity.kind
         end
       end
 
       it "converts mixin type identifiers to objects if a mixin is added to the entities mixins" do
         mixin = 'http://example.com/mynamespace#mymixin'
-        entity = Occi::Core::Entity.new
         entity.mixins << mixin
         expect(entity.mixins.first.to_s).to eq mixin
       end
 
       # TODO: check adding of model
       context 'checking attribute validity' do
-        let(:entity){ Occi::Core::Entity.new }
         it 'fails check with model missing' do
           expect { entity.check }.to raise_error
         end
@@ -58,7 +57,6 @@ module Occi
       end
 
       context 'attributes' do
-        let(:entity){ Occi::Core::Entity.new }
         context '#location' do
           it 'can be set and read' do
             entity.location = 'TestLoc'
@@ -84,6 +82,19 @@ module Occi
 #            entity.title = ''
 #            expect{entity.check}.to raise_error(Occi::Errors::AttributeTypeError)
 #          end
+        end
+
+        context '#actions' do
+          it 'can be populated through redirection' do
+            entity.actions << testaction
+            expect(entity.actions.count).to eq 1
+          end
+          it 'can be assigned through the setter method' do
+            acts = Occi::Core::Actions.new
+            acts << testaction
+            entity.actions=acts
+            expect(entity.actions.count).to eq 1
+          end
         end
       end
 
