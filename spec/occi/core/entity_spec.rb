55888be85d3b709ff087f68453e4ed6bc1f9fc57
Zdenek Sustr <sustr4@cesnet.cz>
2013-10-18 11:14:54 +0200

    Entity specifications updated
    'should' clauses replaced with 'expect'
    individual tests split: 1 test / 1 block

--- spec/occi/core/entity_spec.rb
+++ spec/occi/core/entity_spec.rb
@@ -4,41 +4,57 @@ module Occi
 
       it "initializes itself successfully" do
         entity = Occi::Core::Entity.new
-        entity.should be_kind_of Occi::Core::Entity
+        expect(entity).to be_kind_of Occi::Core::Entity
       end
 
-      it "initializes a subclass successfully using a type identifier" do
-        type_identifier = 'http://example.com/testnamespace#test'
-        entity = Occi::Core::Entity.new type_identifier
-        entity.should be_kind_of 'Com::Example::Testnamespace::Test'.constantize
-        entity.kind.type_identifier.should == type_identifier
-        entity.kind.related.first.should == Occi::Core::Entity.kind
+      context 'initializiation of a subclass using a type identifier' do
+        let(:type_identifier){ 'http://example.com/testnamespace#test' }
+        let(:entity){ Occi::Core::Entity.new type_identifier }
+        it 'has the correct kind' do
+          expect(entity).to be_kind_of 'Com::Example::Testnamespace::Test'.constantize
+        end
+        it 'uses the right identifier' do
+          expect(entity.kind.type_identifier).to eq type_identifier
+        end
+        it 'relates to Entity' do
+          expect(entity.kind.related.first).to eq Occi::Core::Entity.kind
+        end
       end
 
-      it "initializes a subclass successfully using an OCCI Kind" do
-        kind = Occi::Core::Resource.kind
-        entity = Occi::Core::Entity.new kind
-        entity.should be_kind_of Occi::Core::Resource
-        entity.kind.type_identifier.should == Occi::Core::Resource.type_identifier
-        entity.kind.related.first.should == Occi::Core::Entity.kind
+      context "initialization of a subclass using an OCCI Kind" do
+        let(:kind){ Occi::Core::Resource.kind }
+        let(:entity){ Occi::Core::Entity.new kind }
+        it 'has the correct kind' do
+          expect(entity).to be_kind_of Occi::Core::Resource
+        end
+        it 'uses the right identifier' do
+          expect(entity.kind.type_identifier).to eq Occi::Core::Resource.type_identifier
+        end
+        it 'relates to Entity' do
+          expect(entity.kind.related.first).to eq Occi::Core::Entity.kind
+        end
       end
 
       it "converts mixin type identifiers to objects if a mixin is added to the entities mixins" do
         mixin = 'http://example.com/mynamespace#mymixin'
         entity = Occi::Core::Entity.new
         entity.mixins << mixin
-        entity.mixins.first.to_s.should == mixin
+        expect(entity.mixins.first.to_s).to eq mixin
       end
 
       # TODO: check adding of model
-      it "checks a valid attribute value against their definition in its kind and associated mixins" do
-        entity = Occi::Core::Entity.new
-        expect { entity.check }.to raise_error
-        entity.model = Occi::Model.new
-        entity.title = 'test'
-        uuid = UUIDTools::UUID.random_create.to_s
-        entity.id = uuid
-        expect { entity.check }.to_not raise_error
+      context 'checking attribute validity' do
+        let(:entity){ Occi::Core::Entity.new }
+        it 'fails check with model missing' do
+          expect { entity.check }.to raise_error
+        end
+        it 'runs check successfully with a model registered' do
+          entity.model = Occi::Model.new
+          entity.title = 'test'
+          uuid = UUIDTools::UUID.random_create.to_s
+          entity.id = uuid
+          expect { entity.check }.to_not raise_error
+        end
       end
 
     end
