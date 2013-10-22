16a8a914a753a1167a46ca56498fd856fe082d49
Zdenek Sustr <git@sustr.net>
2013-10-22 18:06:36 +0200

    Rest of .resource test files separated

--- spec/occi/parser/text_spec.rb
+++ spec/occi/parser/text_spec.rb
@@ -45,64 +45,49 @@ module Occi
 
       end
 
-      describe '.resource' do
+      context '.resource' do
+        context 'attributes' do
+          let(:resource_string){ File.open("spec/occi/parser/text_samples/occi_resource_w_attributes.text", "rb").read }
+          let(:collection){ Occi::Parser::Text.resource resource_string.lines }
 
-        it 'parses a string with an OCCI Resource including attributes' do
-          resource_string = %Q|Category: compute;scheme="http://schemas.ogf.org/occi/infrastructure#";class="kind"\nCategory: compute;scheme="http://opennebula.org/occi/infrastructure#";class="mixin"|
-          resource_string << %Q|\nCategory: monitoring;scheme="https://occi.carach5.ics.muni.cz/occi/infrastructure/os_tpl#";class="mixin"|
-          resource_string << %Q|\nCategory: small;scheme="https://occi.carach5.ics.muni.cz/occi/infrastructure/resource_tpl#";class="mixin"|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.core.id="ee13808d-7708-4341-a4ba-0e42e4818218"|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.core.title="TestVM"|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.cores=1|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.memory=1.7|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.architecture="x86"|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.speed=1|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.state="active"|
-          resource_string << %Q|\nX-OCCI-Attribute: org.opennebula.compute.cpu=1.0|
-
-          collection = Occi::Parser::Text.resource resource_string.lines
-          collection.resources.first.attributes['occi.compute.cores'].should eq(1)
-          collection.resources.first.attributes['org.opennebula.compute.cpu'].should eq(1.0)
-          collection.resources.first.attributes['occi.compute.architecture'].should eq("x86")
-          collection.resources.first.attributes['occi.compute.memory'].should eq(1.7)
-        end
-
-        it 'parses a string with an OCCI Resource including inline OCCI Link definitions' do
-          resource_string = %Q|Category: compute;scheme="http://schemas.ogf.org/occi/infrastructure#";class="kind"\nCategory: compute;scheme="http://opennebula.org/occi/infrastructure#";class="mixin"|
-          resource_string << %Q|\nCategory: monitoring;scheme="https://occi.carach5.ics.muni.cz/occi/infrastructure/os_tpl#";class="mixin"|
-          resource_string << %Q|\nCategory: small;scheme="https://occi.carach5.ics.muni.cz/occi/infrastructure/resource_tpl#";class="mixin"|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.core.id="ee13808d-7708-4341-a4ba-0e42e4818218"|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.core.title="TestVM"|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.cores=1|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.memory=1.7|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.architecture="x86"|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.speed=1|
-          resource_string << %Q|\nX-OCCI-Attribute: occi.compute.state="active"|
-          resource_string << %Q|\nX-OCCI-Attribute: org.opennebula.compute.cpu=1.0|
-          resource_string << %Q|\nLink: </storage/e60aa2b8-0c86-5973-b93e-30c5c46d6eac>;rel="http://schemas.ogf.org/occi/infrastructure#storage";self="/storagelink/b2f7f1de-c60c-5b08-879c-81f52429c4ef";category="http://schemas.ogf.org/occi/infrastructure#storagelink";occi.core.id="b2f7f1de-c60c-5b08-879c-81f52429c4ef" occi.core.title="link to storage" occi.storagelink.deviceid="xvda" occi.storagelink.state="inactive" org.opennebula.storagelink.bus="ide" org.opennebula.storagelink.driver="tap2:tapdisk:aio:"|
-          resource_string << %Q|\nLink: </network/e4bd81c4-adda-5626-840d-39bb7959db97>;rel="http://schemas.ogf.org/occi/infrastructure#network";self="/networkinterface/e75ab249-9325-511c-82b8-a7e4430381e3";category="http://schemas.ogf.org/occi/infrastructure#networkinterface";occi.core.id="e75ab249-9325-511c-82b8-a7e4430381e3" occi.core.title="link to network interface" occi.networkinterface.address="192.168.254.8" occi.networkinterface.mac="02:00:c0:a8:fe:08" occi.networkinterface.state="inactive" org.opennebula.networkinterface.bridge="xenbr0"|
-
-          collection = Occi::Parser::Text.resource resource_string.lines
-          collection.resources.should have(1).resource
-          collection.links.should have(2).links
-        end
-
-        context '#resource' do
-          context 'inline Links and Mixins' do
-            let(:resource_string){ File.open("spec/occi/parser/text_samples/occi_resource_w_inline_links.text", "rb").read }
-            let(:collection){ Occi::Parser::Text.resource resource_string.lines }
-
-            it 'has the right number of resources' do
-              expect(collection.resources).to have(1).resource
-            end
-            it 'has the right number of links' do
-              expect(collection.links).to have(5).links
-            end
+          it 'parses No. of cores' do
+            expect(collection.resources.first.attributes['occi.compute.cores']).to eq 1
+          end
+          it 'parses compute cpu' do
+            expect(collection.resources.first.attributes['org.opennebula.compute.cpu']).to eq 1.0
+          end
+          it 'parses architecture' do
+            expect(collection.resources.first.attributes['occi.compute.architecture']).to eq "x86"
+          end
+          it 'parses compute memory' do
+            expect(collection.resources.first.attributes['occi.compute.memory']).to eq 1.7
           end
         end
 
-      end
+        context 'inline links' do
+          let(:resource_string){ File.open("spec/occi/parser/text_samples/occi_resource_w_inline_links_only.text", "rb").read }
+          let(:collection){ Occi::Parser::Text.resource resource_string.lines }
 
+          it 'has the right number of resources' do
+            expect(collection.resources).to have(1).resource
+          end
+          it 'has the right number of links' do
+            expect(collection.links).to have(2).links
+          end
+        end
+
+        context 'inline Links and Mixins' do
+          let(:resource_string){ File.open("spec/occi/parser/text_samples/occi_resource_w_inline_links.text", "rb").read }
+          let(:collection){ Occi::Parser::Text.resource resource_string.lines }
+
+          it 'has the right number of resources' do
+            expect(collection.resources).to have(1).resource
+          end
+          it 'has the right number of links' do
+            expect(collection.links).to have(5).links
+          end
+        end
+      end
     end
   end
 end
