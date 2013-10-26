afb3290a3bcd75491a7b9dc05d29ff4e6ed842f6
Boris Parak <parak@cesnet.cz>
2013-10-26 19:21:42 +0200

    Added stubs for attributes specs

--- spec/occi/core/attributes_spec.rb
+++ spec/occi/core/attributes_spec.rb
@@ -2,39 +2,88 @@ module Occi
   module Core
     describe Attributes do
 
-      it 'stores properties using hashes in hash notation'  do
-        attributes=Occi::Core::Attributes.new
-        attributes['test']={}
-        attributes['test'].should be_kind_of Occi::Core::Properties
+      context '#[]=' do
+        it 'stores properties using hashes in hash notation'  do
+          attributes=Occi::Core::Attributes.new
+          attributes['test']={}
+          
+          expect(attributes['test']).to be_kind_of Occi::Core::Properties
+        end
+
+        it 'stores properties using hashes in dot notation'  do
+          attributes=Occi::Core::Attributes.new
+          attributes.test={}
+
+          expect(attributes.test).to be_kind_of Occi::Core::Properties
+        end
       end
 
-      it 'stores properties using hashes in dot notation'  do
-        attributes=Occi::Core::Attributes.new
-        attributes.test={}
-        attributes.test.should be_kind_of Occi::Core::Properties
+      context '#remove' do
+        it 'removes attributes' do
+          attributes=Occi::Core::Attributes.new
+          attributes['one.two']={}
+          attributes['one.three']={}
+
+          attr=Occi::Core::Attributes.new
+          attr['one.two']={}
+          attributes.remove attr
+
+          expect(attributes['one.two']).to be_nil
+          expect(attributes['one.three']).to be_kind_of Occi::Core::Properties
+        end
       end
 
-      it 'removes attributes' do
-        attributes=Occi::Core::Attributes.new
-        attributes['one.two']={}
-        attributes['one.three']={}
-        attr=Occi::Core::Attributes.new
-        attr['one.two']={}
-        attributes.remove attr
-        attributes['one.two'].should be_nil
-        attributes['one.three'].should be_kind_of Occi::Core::Properties
+      context '#convert' do
+        it 'converts properties to an empty attribute' do
+          attributes=Occi::Core::Attributes.new
+          attributes.test={}
+
+          attr = attributes.convert
+          expect(attributes.test).to be_kind_of Occi::Core::Properties
+
+          expect(attr.test).to be_nil
+          expect(attr._test).to be_kind_of Occi::Core::Properties
+
+          attributes.convert!
+          expect(attributes.test).to be_nil
+          expect(attributes._test).to be_kind_of Occi::Core::Properties
+        end
       end
 
-      it 'converts properties to an empty attribute' do
-        attributes=Occi::Core::Attributes.new
-        attributes.test={}
-        attr = attributes.convert
-        attributes.test.should be_kind_of Occi::Core::Properties
-        attr.test.should be_nil
-        attr._test.should be_kind_of Occi::Core::Properties
-        attributes.convert!
-        attributes.test.should be_nil
-        attributes._test.should be_kind_of Occi::Core::Properties
+      context '#==' do
+
+        it 'matches the same instance'
+        it 'matches a clone'
+        it 'matches a new instance with the same content'
+        it 'does not match nil'
+        it 'does not match an instance with different content'
+
+      end
+
+      context '#eql?' do
+
+        it 'matches the same instance'
+        it 'matches a clone'
+        it 'matches a new instance with the same content'
+        it 'does not match nil'
+        it 'does not match an instance with different content'
+
+      end
+
+      context '#equal?' do
+
+        it 'matches the same instance'
+        it 'does not match a clone'
+
+      end
+
+      context '#hash' do
+
+        it 'matches for the same instance'
+        it 'matches for a clone'
+        it 'matches for a new instance with the same content'
+        it 'does not match for an instance with different content'
+
       end
 
     end
