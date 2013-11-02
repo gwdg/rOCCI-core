c56186c3ef4d966fd71888e62b6d2998493531ca
Zdenek Sustr <git@sustr.net>
2013-11-02 07:38:06 +0100

    Collection merge() specs

--- spec/occi/collection_spec.rb
+++ spec/occi/collection_spec.rb
@@ -78,5 +78,158 @@ module Occi
       end
     end
 
+    context '#merge' do
+      before(:each) {
+        collection.kinds << "http://schemas.ogf.org/occi/infrastructure#compute"
+        collection.mixins << "http://example.com/occi/tags#my_mixin"
+        collection.actions << "http://schemas.ogf.org/occi/infrastructure/compute/action#start"
+        collection.action = Occi::Core::ActionInstance.new
+        collection.resources << Occi::Core::Resource.new
+        collection.links << Occi::Core::Link.new
+      }
+      context 'two fully initiated collections' do
+        let(:action){ Occi::Core::Action.new scheme='http://schemas.ogf.org/occi/core/entity/action#', term='testaction', title='testaction action' }
+        let(:coll2){
+          coll2 = Occi::Collection.new
+          coll2.kinds << "http://schemas.ogf.org/occi/infrastructure#storage"
+          coll2.mixins << "http://example.com/occi/tags#another_mixin"
+          coll2.actions << "http://schemas.ogf.org/occi/infrastructure/compute/action#stop"
+          coll2.action = Occi::Core::ActionInstance.new action
+          coll2.resources << Occi::Core::Resource.new
+          coll2.links << Occi::Core::Link.new
+          coll2
+        }
+#        let(:merged){ collection.merge(coll2, collection) }
+        let(:merged){ collection.merge(coll2) }
+
+        context 'resulting collection' do
+          it 'has the correct number of kinds' do
+            expect(merged.kinds.count).to eql 2
+          end
+
+          it 'has the correct number of mixins' do
+            expect(merged.mixins.count).to eql 2
+          end
+
+          it 'has the correct number of actions' do
+            expect(merged.actions.count).to eql 2
+          end
+
+          it 'has the correct number of resources' do
+            expect(merged.resources.count).to eql 2
+          end
+
+          it 'has the correct number of links' do
+            expect(merged.links.count).to eql 2
+          end
+
+          it 'inherits action from the other collection' do
+            expect(merged.action.action.term).to eql "testaction"
+          end
+
+          it 'does not inherit action from first collection' do
+            expect(merged.action.action.term).to_not eql "action_instance"
+          end
+
+          it 'holds kinds from first collection' do
+            expect(collection.kinds.subset?(merged.kinds)).to eql true
+          end
+
+          it 'holds mixins from first collection' do
+            expect(collection.mixins.subset?(merged.mixins)).to eql true
+          end
+
+          it 'holds actions from first collection' do
+            expect(collection.actions.subset?(merged.actions)).to eql true
+          end
+
+          it 'holds resources from first collection' do
+            expect(collection.resources.subset?(merged.resources)).to eql true
+          end
+
+          it 'holds links from first collection' do
+            expect(collection.links.subset?(merged.links)).to eql true
+          end
+
+          it 'holds kinds from other collection' do
+            expect(coll2.kinds.subset?(merged.kinds)).to eql true
+          end
+
+          it 'holds mixins from other collection' do
+            expect(coll2.mixins.subset?(merged.mixins)).to eql true
+          end
+
+          it 'holds actions from other collection' do
+            expect(coll2.actions.subset?(merged.actions)).to eql true
+          end
+
+          it 'holds resources from other collection' do
+            expect(coll2.resources.subset?(merged.resources)).to eql true
+          end
+
+          it 'holds links from other collection' do
+            expect(coll2.links.subset?(merged.links)).to eql true
+          end
+
+          it 'does not replace first collection' do
+            expect(merged).to_not eql collection
+          end
+        end
+
+        context 'first original' do
+          it 'kept the correct number of kinds' do
+            expect(collection.kinds.count).to eql 1
+          end
+
+          it 'kept the correct number of mixins' do
+            expect(collection.mixins.count).to eql 1
+          end
+
+          it 'kept the correct number of actions' do
+            expect(collection.actions.count).to eql 1
+          end
+
+          it 'kept the correct number of resources' do
+            expect(collection.resources.count).to eql 1
+          end
+
+          it 'kept the correct number of links' do
+            expect(collection.links.count).to eql 1
+          end
+        end
+
+        context 'second original' do
+          it 'kept the correct number of kinds' do
+            expect(coll2.kinds.count).to eql 1
+          end
+
+          it 'kept the correct number of mixins' do
+            expect(coll2.mixins.count).to eql 1
+          end
+        
+          it 'kept the correct number of actions' do
+            expect(coll2.actions.count).to eql 1
+          end
+
+          it 'kept the correct number of resources' do
+            expect(coll2.resources.count).to eql 1
+          end
+
+          it 'kept the correct number of links' do
+            expect(coll2.links.count).to eql 1
+          end
+        end
+      end
+
+      it 'copes with en empty collection' do
+        emptycol = Occi::Collection.new
+        expect{merged = collection.merge(emptycol, collection)}.to_not raise_error
+      end
+
+
+      it 'copes with both collections empty'
+
+    end
+
   end
 end
