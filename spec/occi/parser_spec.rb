261a3c8cac2a2587bcd4251b3e8292a5d5382b3f
Zdenek Sustr <git@sustr.net>
2013-11-11 14:25:23 +0100

    "Better specs" applied to parser test
       * Test files for parser moved deeper
       * Relying more on Marshal

--- spec/occi/parser_spec.rb
+++ spec/occi/parser_spec.rb
@@ -1,164 +1,118 @@
 module Occi
   describe "Parser" do
 
-    it "parses an OCCI message with MIME type text/plain containing an OCCI resource" do
-      # create new collection
-      collection = Occi::Collection.new
-      # create new resource within collection
-      resource = collection.resources.create
+    context ".parse" do
 
-      # render collection to text/plain MIME type
-      rendered_collection = collection.to_text
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('text/plain', rendered_collection).to_json.should == collection.to_json
+      context "render->parse tests" do
+        let(:collection){ Occi::Collection.new }
+        let(:resource){ resource = collection.resources.create }
+        let(:link){ link = resource.links.create
+            link.id = UUIDTools::UUID.random_create.to_s
+            link.target = 'http://example.com/resource/aee5acf5-71de-40b0-bd1c-2284658bfd0e'
+            link.source = resource
+            link
+        }
 
-      # add attributes to resource
-      resource.id = UUIDTools::UUID.random_create.to_s
-      resource.title = 'title'
+        context "resources from OCCI messages with text/plain MIME type" do
+          let(:rendered_collection){ collection.to_text }
 
-      # render collection to text/plain MIME type
-      rendered_collection = collection.to_text
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('text/plain', rendered_collection).to_json.should == collection.to_json
+          it 'parses self-generated collection with resources' do
+            expect(Occi::Parser.parse('text/plain', rendered_collection).to_json).to eql collection.to_json
+          end
 
-      # add mixin to resource
-      resource.mixins << Occi::Core::Mixin.new
+          it 'parses self-generated collection with added attributes' do
+            resource.id = UUIDTools::UUID.random_create.to_s
+            resource.title = 'title'
+            expect(Occi::Parser.parse('text/plain', rendered_collection).to_json).to eql collection.to_json
+          end
 
-      # render collection to text/plain MIME type
-      rendered_collection = collection.to_text
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('text/plain', rendered_collection).to_json.should == collection.to_json
+          it 'parses self-generated collection with added mixin' do
+            resource.mixins << Occi::Core::Mixin.new
+            expect(Occi::Parser.parse('text/plain', rendered_collection).to_json).to eql collection.to_json
+          end
 
-      # add link to resource
-      link = resource.links.create
-      link.id = UUIDTools::UUID.random_create.to_s
-      link.target = 'http://example.com/resource/aee5acf5-71de-40b0-bd1c-2284658bfd0e'
-      link.source = resource
-      collection << link
+          it 'parses self-generated collection with added link' do
+            collection << link
+            expect(Occi::Parser.parse('text/plain', rendered_collection).to_json).to eql collection.to_json
+          end
+        end
 
-      # render collection to text/plain MIME type
-      rendered_collection = collection.to_text
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('text/plain', rendered_collection).to_json.should == collection.to_json
-    end
+        context 'resources from OCCI messages with text/occi MIME type' do
+          let(:rendered_collection){ collection.to_header }
 
-    it "parses an OCCI message with MIME type text/occi containing an OCCI resource" do
-      # create new collection
-      collection = Occi::Collection.new
-      # create new resource within collection
-      resource = collection.resources.create
+          it 'parses self-generated collection with resources' do
+            expect(Occi::Parser.parse('text/occi', '', false, Occi::Core::Resource, rendered_collection).to_header).to eql collection.to_header
+          end
 
-      # render collection to text/occi MIME type
-      rendered_collection = collection.to_header
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('text/occi', '', false, Occi::Core::Resource, rendered_collection).to_header.should == collection.to_header
+          it 'parses self-generated collection with added attributes' do
+            resource.id = UUIDTools::UUID.random_create.to_s
+            resource.title = 'title'
+            expect(Occi::Parser.parse('text/occi', '', false, Occi::Core::Resource, rendered_collection).to_header).to eql collection.to_header
+          end
 
-      # add attributes to resource
-      resource.id = UUIDTools::UUID.random_create.to_s
-      resource.title = 'title'
+          it 'parses self-generated collection with added mixin' do
+            resource.mixins << Occi::Core::Mixin.new
+            expect(Occi::Parser.parse('text/occi', '', false, Occi::Core::Resource, rendered_collection).to_header).to eql collection.to_header
+          end
 
-      # render collection to text/occi MIME type
-      rendered_collection = collection.to_header
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('text/occi', '', false, Occi::Core::Resource, rendered_collection).to_header.should == collection.to_header
+          it 'parses self-generated collection with added link' do
+            collection << link
+            expect(Occi::Parser.parse('text/occi', '', false, Occi::Core::Resource, rendered_collection).to_header).to eql collection.to_header
+          end
+        end
 
-      # add mixin to resource
-      resource.mixins << Occi::Core::Mixin.new
+        context 'resources from OCCI messages with application/occi+json MIME type' do
+          let(:rendered_collection){ collection.to_json }
+          it 'parses self-generated collection with resources' do
+            expect(Occi::Parser.parse('application/occi+json', rendered_collection)).to eql collection
+          end
 
-      # render collection to text/occi MIME type
-      rendered_collection = collection.to_header
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('text/occi', '', false, Occi::Core::Resource, rendered_collection).to_header.should == collection.to_header
+          it 'parses self-generated collection with added attributes' do
+            resource.id = UUIDTools::UUID.random_create.to_s
+            resource.title = 'title'
+            expect(Occi::Parser.parse('application/occi+json', rendered_collection)).to eql collection
+          end
 
-      # add link to resource
-      link = resource.links.create
-      link.id = UUIDTools::UUID.random_create.to_s
-      link.target = 'http://example.com/resource/aee5acf5-71de-40b0-bd1c-2284658bfd0e'
-      link.source = resource
-      collection << link
+          it 'parses self-generated collection with added mixin' do
+            resource.mixins << Occi::Core::Mixin.new
+            expect(Occi::Parser.parse('application/occi+json', rendered_collection)).to eql collection
+          end
 
-      # render collection to text/occi MIME type
-      rendered_collection = collection.to_header
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('text/occi', '', false, Occi::Core::Resource, rendered_collection).to_header.should == collection.to_header
-    end
+          it 'parses self-generated collection with added link' #do #Already deactivated in previous version
+#            collection << link
+#            expect(Occi::Parser.parse('application/occi+json', rendered_collection)).to eql collection
+#          end
 
-    it "parses an OCCI message with MIME type application/occi+json containing an OCCI resource" do
-      # create new collection
-      collection = Occi::Collection.new
-      # create new resource within collection
-      resource = collection.resources.create
-
-      # render collection to text/plain MIME type
-      rendered_collection = collection.to_json
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('application/occi+json', rendered_collection).should == collection
-
-      # add attributes to resource
-      resource.id = UUIDTools::UUID.random_create.to_s
-      resource.title = 'title'
-
-      # render collection to text/plain MIME type
-      rendered_collection = collection.to_json
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('application/occi+json', rendered_collection).should == collection
-
-      # add mixin to resource
-      resource.mixins << Occi::Core::Mixin.new
-
-      # render collection to text/plain MIME type
-      rendered_collection = collection.to_json
-      # parse rendered collection and compare with original collection
-      Occi::Parser.parse('application/occi+json', rendered_collection).should == collection
-
-      # add link to resource
-      link = resource.links.create
-      link.target = '/resource/aee5acf5-71de-40b0-bd1c-2284658bfd0e'
-      link.source = resource.location
-
-      # render collection to text/plain MIME type
-      rendered_collection = collection.to_json
-      # parse rendered collection and compare with original collection
-      # TODO: Temporarily deactivating
-#      Occi::Parser.parse('application/occi+json', rendered_collection).should == collection
+        end
+      end
     end
 
     it "parses an OVF file" do
       media_type = 'application/ovf+xml'
-      body = File.read('spec/occi/test.ovf')
+      body = File.read('spec/occi/parser/ovf_samples/test.ovf')
       collection = Occi::Parser.parse(media_type, body)
-      storage_resources = collection.resources.select { |resource| resource.kind.to_s == 'http://schemas.ogf.org/occi/infrastructure#storage' }
-      storage_resources.should have(1).storage_resource
-      storage_resources.first.title.should == 'lamp'
-      network_resources = collection.resources.select { |resource| resource.kind.to_s == 'http://schemas.ogf.org/occi/infrastructure#network' }
-      network_resources.should have(1).network_resource
-      network_resources.first.title.should == 'VM Network'
-      compute_resources = collection.resources.select { |resource| resource.kind.to_s == 'http://schemas.ogf.org/occi/infrastructure#compute' }
-      compute_resources.should have(1).compute_resource
-      compute_resources.first.attributes.occi!.compute!.cores.should == 1
-      compute_resources.first.attributes.occi!.compute!.memory.should == 0.25
+
+      collection.resources.each { |res| res.id="noid" } #Equalize auto-generated IDs
+
+      expected = Marshal.load(File.open("spec/occi/parser/ovf_samples/test.dump", "rb"))
+      expect(collection).to eql expected
     end
 
     it "parses an OVA container" do
       media_type = 'application/ova'
-      body = File.read('spec/occi/test.ova')
+      body = File.read('spec/occi/parser/ova_samples/test.ova')
       collection = Occi::Parser.parse(media_type, body)
-      storage_resources = collection.resources.select { |resource| resource.kind.to_s == 'http://schemas.ogf.org/occi/infrastructure#storage' }
-      storage_resources.should have(1).storage_resource
-      storage_resources.first.title.should == 'lamp'
-      network_resources = collection.resources.select { |resource| resource.kind.to_s == 'http://schemas.ogf.org/occi/infrastructure#network' }
-      network_resources.should have(1).network_resource
-      network_resources.first.title.should == 'VM Network'
-      compute_resources = collection.resources.select { |resource| resource.kind.to_s == 'http://schemas.ogf.org/occi/infrastructure#compute' }
-      compute_resources.should have(1).compute_resource
-      compute_resources.first.attributes.occi!.compute!.cores.should == 1
-      compute_resources.first.attributes.occi!.compute!.memory.should == 0.25
+
+      collection.resources.each { |res| res.id="noid" } #Equalize auto-generated IDs
+
+      expected = Marshal.load(File.open("spec/occi/parser/ova_samples/test.dump", "rb"))
+      expect(collection).to eql expected
     end
 
 #    ZS 11 Oct 2013: XML format not yet properly specified
 #    it "parses a XML file" do
 #      media_type = 'application/xml'
-#      body = File.read('spec/occi/test.xml')
+#      body = File.read('spec/occi/parser/xml_samples/test.xml')
 #      collection = Occi::Parser.parse(media_type, body)
 #      
 #    end
