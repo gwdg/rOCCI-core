214ac13d5d82d6743b294a903442bf5dad4344c5
Zdenek Sustr <git@sustr.net>
2013-11-22 11:20:25 +0100

    Avoid spawning a new class, use regular link kind

--- spec/occi/core/link_spec.rb
+++ spec/occi/core/link_spec.rb
@@ -12,16 +12,16 @@ module Occi
           defs['stringtype'] =   { :type => 'string', :pattern => '[adefltuv]+',
                                            :default => 'defaultforlink', :mutable => true, :required => true }
           defs }
-        let(:kind){ Occi::Core::Kind.new 'http://schemas.ogf.org/occi/core#', 'testkind', 'Test Kind', defs }
-        let(:model){ model = Occi::Model.new
-          model.register(kind)
-          model }
 
         it 'sets default for required attribute' do
-          lnk = Occi::Core::Link.new(kind, [], defs)
-          lnk.model = model
-          lnk.check
-          expect(lnk.attributes['stringtype']).to eql 'defaultforlink'
+          link = Occi::Core::Link.new
+          link.kind.attributes.merge!(defs)
+          model = Occi::Model.new
+          model.register(link.kind)
+          link.model = model
+
+          link.check
+          expect(link.attributes['stringtype']).to eql 'defaultforlink'
         end
 
         it 'raises error if no relationship is set' do
