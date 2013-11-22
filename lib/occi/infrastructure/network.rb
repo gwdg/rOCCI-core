bb9e1ccabd41cee2d8b5897467e67a814e4b27b7
Zdenek Sustr <git@sustr.net>
2013-11-22 14:38:19 +0100

    Fixes in attributes (obvious errors :-/ )

--- lib/occi/infrastructure/network.rb
+++ lib/occi/infrastructure/network.rb
@@ -15,10 +15,11 @@ module Occi
       self.attributes = Occi::Core::Attributes.new(Occi::Core::Resource.attributes)
       self.attributes['occi.network.vlan'] = {:type => 'number',
                                               :mutable => true,
-                                              :pattern => 'x86|x64'}
-      self.attributes['occi.network.label'] = {:type => 'number',
+                                              :pattern => '\d+'}
+      self.attributes['occi.network.label'] = {:type => 'string',
                                                :mutable => true}
-      self.attributes['occi.network.state'] ={:pattern => 'active|inactive|error',
+      self.attributes['occi.network.state'] ={:type => 'string',
+                                              :pattern => 'active|inactive|error',
                                               :default => 'inactive'}
 
       self.kind = Occi::Core::Kind.new scheme='http://schemas.ogf.org/occi/infrastructure#',
