1b2c10852be5a4d4bf292e37aac1f015f95b4f6e
Boris Parak <parak@cesnet.cz>
2014-09-29 20:25:00 +0200

    [RSpec] Clean-up in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,17 +13,6 @@ require 'json_spec'
 
 Dir["#{File.dirname(__FILE__)}/helpers/*.rb"].each {|file| require file }
 
-# simplify the usage of VCR; this will allow us to use
-#
-#   it "does something", :vcr do
-#     ...
-#   end
-#
-# instead of
-#
-#   it "does something else", :vcr => true do
-#     ...
-#   end
 RSpec.configure do |c|
   c.include JsonSpec::Helpers
 end
