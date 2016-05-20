fc0d24333a82f2e72f1e817895f29272671f9ef5
Boris Parak <parak@cesnet.cz>
2016-05-20 16:47:47 +0200

    [Next] Rough outline of Category

--- lib/occi/core/category.rb
+++ lib/occi/core/category.rb
@@ -1,6 +1,45 @@
 module Occi
   module Core
     #
-    class Category; end
+    class Category
+
+      attr_accessor :term, :schema, :title, :attribute_definitions
+
+      def initialize(args = {})
+        pre_initialize
+
+        args.merge!(defaults) { |_, oldval, _| oldval }
+        @term = args.fetch(:term)
+        @schema = args.fetch(:schema)
+        @title = args.fetch(:title)
+        @attribute_definitions = args.fetch(:attribute_definitions)
+
+        post_initialize
+      end
+
+      def identifier
+        "#{schema}#{term}"
+      end
+
+      def valid?; end
+
+      def validate; end
+
+      def validate!; end
+
+      protected
+
+      #
+      def defaults
+        { title: '', attribute_definitions: AttributeDefinitions.new }
+      end
+
+      #
+      def pre_initialize; end
+
+      #
+      def post_initialize; end
+
+    end
   end
 end
--- spec/occi/core/category_spec.rb
+++ spec/occi/core/category_spec.rb
@@ -2,33 +2,135 @@ module Occi
   module Core
     describe Category do
 
-      it 'has term'
-      it 'has schema'
-      it 'has title'
-      it 'has identifier'
-      it 'has attribute definitions'
+      before(:each) do
+        @category = Category.new(
+          term: 'generic',
+          schema: 'http://schemas.org/schema#',
+          title: 'Generic category',
+          attribute_definitions: double("attribute_definitions")
+        )
+      end
 
-      it 'allows the change of term'
-      it 'allows the change of schema'
-      it 'allows the change of time'
-      it 'does not allow the change of identifier'
-      it 'allows the change of attribute definitions'
+      it 'has term' do
+        expect(@category.term).to eq 'generic'
+      end
 
-      it 'can validate term by returning boolean'
-      it 'can validate schema by returning boolean'
-      it 'can validate term by raising an error'
-      it 'can validate schema by raising an error'
+      it 'has schema' do
+        expect(@category.schema).to eq 'http://schemas.org/schema#'
+      end
+
+      it 'has title' do
+        expect(@category.title).to eq 'Generic category'
+      end
+
+      it 'has identifier' do
+        expect(@category.identifier).to eq 'http://schemas.org/schema#generic'
+      end
+
+      it 'has attribute definitions' do
+        expect(@category.attribute_definitions).not_to be_nil
+      end
+
+      it 'allows the change of term' do
+        expect { @category.term = 'nope' }.not_to raise_error
+        expect(@category.term).to eq 'nope'
+      end
+
+      it 'allows the change of schema' do
+        expect { @category.schema = 'http://schemas.org/nope#' }.not_to raise_error
+        expect(@category.schema).to eq 'http://schemas.org/nope#'
+      end
+
+      it 'allows the change of title' do
+        expect { @category.title = 'Nope category' }.not_to raise_error
+        expect(@category.title).to eq 'Nope category'
+      end
+
+      it 'does not allow the change of identifier' do
+        expect { @category.identifier = 'http://schemas.org/nope#nope' }.to raise_error(NoMethodError)
+        expect(@category.identifier).to eq 'http://schemas.org/schema#generic'
+      end
+
+      it 'allows the change of attribute definitions' do
+        old_attribute_definitions = @category.attribute_definitions
+        expect { @category.attribute_definitions = double("attribute_definitions") }.not_to raise_error
+        expect(@category.attribute_definitions).not_to equal old_attribute_definitions
+      end
+
+      it 'is invalid without term' do
+        @category.term = nil
+        expect(@category.valid?).to be false
+      end
+
+      it 'is invalid with empty term' do
+        @category.term = ''
+        expect(@category.valid?).to be false
+      end
+
+      it 'is invalid with invalid term' do
+        @category.term = '*%^3*5636 43 32456//'
+        expect(@category.valid?).to be false
+      end
+
+      it 'is invalid without schema' do
+        @category.schema = nil
+        expect(@category.valid?).to be false
+      end
+
+      it 'is invalid with empty schema' do
+        @category.schema = ''
+        expect(@category.valid?).to be false
+      end
+
+      it 'is invalid with invalid schema' do
+        @category.schema = '%$4565+ 25 2+65 //=='
+        expect(@category.valid?).to be false
+      end
+
+      it 'is invalid without attribute definitions' do
+        @category.attribute_definitions = nil
+        expect(@category.valid?).to be false
+      end
+
+      it 'is valid with valid term, schema, title, and attribute definitions' do
+        expect(@category.valid?).to be true
+      end
+
+      it 'is valid without title' do
+        @category.title = nil
+        expect(@category.valid?).to be true
+      end
+
+      it 'is valid with attribute definitions' do
+        expect(@category.attribute_definitions).not_to be_nil
+        expect(@category.valid?).to be true
+      end
+
+      it 'passes validation when valid' do
+        expect(@category.valid?).to be true
+        expect(@category.validate).to be true
+      end
+
+      it 'does not pass validation when invalid' do
+        @category.term = nil
+        expect(@category.valid?).to be false
+        expect(@category.validate).to be false
+      end
+
+      it 'raises an error when validated with bang and invalid' do
+        @category.term = nil
+        expect(@category.valid?).to be false
+        expect { @category.validate! }.to raise_error
+      end
+
+      it 'does not raise an error when validated with bang and valid' do
+        expect(@category.valid?).to be true
+        expect { @category.validate! }.not_to raise_error
+      end
 
       it 'cannot be directly rendered'
       it 'can be checked for emptiness'
-      it 'can be validated by returning boolean'
-      it 'can be validated by raising an error'
       it 'can be compared with another instance'
-
-      it 'is invalid without term'
-      it 'is invalid without schema'
-      it 'is invalid with empty term'
-      it 'is invalid with empty schema'
 
     end
   end
