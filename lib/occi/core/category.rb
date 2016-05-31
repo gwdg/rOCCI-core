2d70299a5da50c3602e408820127ba7a22fe0bd5
Boris Parak <parak@cesnet.cz>
2016-05-31 15:52:24 +0200

    [Next] First attempt at Category

--- lib/occi/core/category.rb
+++ lib/occi/core/category.rb
@@ -42,14 +42,48 @@ module Occi
       end
 
       class << self
-        #
-        def valid_term?(term); end
+        PROHIBITED_SCHEMA_CHARS = %w(% & ? ! \\).freeze
+
+        REGEXP_ALPHA = /[a-zA-Z]/
+        REGEXP_DIGIT = /[0-9]/
+        REGEXP_TERM = /(#{REGEXP_ALPHA}|#{REGEXP_DIGIT})(#{REGEXP_ALPHA}|#{REGEXP_DIGIT}|-|_)*/
 
         #
-        def valid_schema?(schema); end
+        def valid_term?(term)
+          !term.blank? && REGEXP_TERM.match(term)
+        end
 
         #
-        def valid_identifier?(identifier); end
+        def valid_schema?(schema)
+          !schema.blank? && valid_uri?(schema) && schema.include?('#') && !has_prohibited_chars?(schema)
+        end
+
+        #
+        def valid_identifier?(identifier)
+          return false if identifier.blank?
+
+          schema, term = identifier.split('#')
+          valid_schema?(schema) && valid_term?(term)
+        end
+
+        #
+        def valid_uri?(uri)
+          begin
+            URI.split(uri)
+          rescue URI::InvalidURIError => ex
+            logger.debug "URI validation: #{ex.message}"
+            return false
+          end
+
+          true
+        end
+
+        private
+
+        #
+        def has_prohibited_chars?(schema)
+          PROHIBITED_SCHEMA_CHARS.collect { |char| schema.include?(char) }.reduce(:&)
+        end
       end
 
       protected
@@ -58,7 +92,7 @@ module Occi
       def sufficient_args!(args)
         [:term, :schema].each do |attr|
           fail Occi::Core::Errors::MandatoryArgumentError,
-               "#{attr} is a mandatory argument" if args[attr].nil?
+               "#{attr} is a mandatory argument" unless self.class.send("valid_#{attr}?", args[attr])
         end
       end
 
