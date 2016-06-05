683c73f64e13ed4a56b7e055d4cc0d4801317342
Boris Parak <parak@cesnet.cz>
2016-06-05 21:09:25 +0200

    [Next] Spec draft & docs for Renderable

--- lib/occi/core/helpers/renderable.rb
+++ lib/occi/core/helpers/renderable.rb
@@ -8,34 +8,77 @@ module Occi
         # Parent namespace of all supported renderer classes
         RENDERER_NAMESPACE = Occi::Core::Renderers
 
-        # @param format [String]
-        # @param options [Hash]
+        # Renders the receiver into the specified over-the-wire `format`.
+        # `format` is automatically injected into `options` and these
+        # are then passed (unmodified) to the underlying renderer. See
+        # documentation of a specific renderer for details.
+        #
+        # @example
+        #   kind.render 'text' # => String
+        #   kind.render 'json' # => String
+        #   kind.render 'headers' # => Hash
+        #
+        # @param format [String] over-the-wire format, mandatory
+        # @param options [Hash] options passed to the underlying renderer
+        # @return [Object] output of the chosen renderer
         def render(format, options = {})
+          raise Occi::Core::RenderingError,
+                'Rendering to an unspecified format is not allowed' if format.blank?
           options[:format] = format
           available_renderers[format].render(self, options)
         end
 
-        # @param base [Class]
+        # Adds available rendering formats as `to_<format>` methods on
+        # the receiver.
+        #
+        # @example
+        #   class Test
+        #     include Renderable
+        #   end
+        #
+        #   t = Test.new
+        #   t.to_text   # => String
+        #
+        # @param base [Class] class receiving this module
         def self.included(base)
           available_formats.each do |format|
             base.send(:define_method, "to_#{format}", proc { render(format) })
           end
         end
 
+        # Allows calling `.extend(Renderable)` on instances. Does not allow
+        # class-based extension, will raise a `RuntimeError` error.
         #
-        # @param base [Class, Object]
+        # @example
+        #   o = Object.new
+        #   o.extend Renderable
+        #   o.to_text  # => String
+        #
+        # @param base [Class, Object] class or object being extended
         def self.extended(base)
           base.is_a?(Class) ? raise("#{self} cannot extend #{base}") : included(base.class)
         end
 
-        # @return [Array]
+        # Lists available rendering `format`s.
+        #
+        # @example
+        #   available_formats # => ['text', 'json', 'headers']
+        #
+        # @return [Array] list of formats, as Strings
         def self.available_formats
           available_renderers.keys
         end
 
-        # @return [Hash]
+        # Lists available renderers, as a Hash mapping `format` to `Renderer` class.
+        #
+        # @example
+        #   available_renderers # => { 'text' => TextRenderer }
+        #
+        # @return [Hash] map of available renderers, keyed by `format`
         def self.available_renderers
-          rcands = renderer_candidates.select { |candidate| renderer?(candidate) }
+          rcands = renderer_candidates(renderer_namespace).select do |candidate|
+            renderer?(candidate, required_renderer_methods)
+          end
 
           ravail = {}
           rcands.each do |rcand|
@@ -45,25 +88,40 @@ module Occi
           ravail
         end
 
-        # @param candidate [Object]
-        def self.renderer?(candidate)
+        # Checks whether the given object can act as a renderer.
+        #
+        # @example
+        #   renderer?(TextRenderer, [:renderer?, :render, :formats]) # => true
+        #   renderer?(NilClass, [:renderer?, :render, :formats]) # => false
+        #
+        # @param candidate [Object] object to check
+        # @param required_renderer_methods [Array] required method symbols
+        def self.renderer?(candidate, required_renderer_methods)
           return false unless candidate.is_a?(Class)
 
           required_renderer_methods.each { |method| return false unless candidate.respond_to?(method) }
           candidate.renderer?
         end
 
-        # @return [Array]
-        def self.renderer_candidates
+        # Returns all renderer candidates from the given namespace. The list may contain
+        # other constants from the given namespace and needs to be refined further.
+        #
+        # @param renderer_namespace [Module] base namespace
+        # @return [Array] list of candidates
+        def self.renderer_candidates(renderer_namespace)
           renderer_namespace.constants.collect { |const| renderer_namespace.const_get(const) }
         end
 
-        # @return [Array]
+        # Lists default methods required from any supported renderer.
+        #
+        # @return [Array] list of method symbols
         def self.required_renderer_methods
           REQUIRED_RENDERER_METHODS
         end
 
-        # @return [Module]
+        # Returns the default renderer namespace.
+        #
+        # @return [Module] base namespace
         def self.renderer_namespace
           RENDERER_NAMESPACE
         end
--- spec/occi/core/helpers/renderable_spec.rb
+++ spec/occi/core/helpers/renderable_spec.rb
@@ -2,7 +2,7 @@ module Occi
   module Core
     module Helpers
       describe Renderable do
-        subject { renderable_object }
+        subject { Renderable }
 
         let(:renderable_object) do
           object = Object.new
@@ -18,14 +18,49 @@ module Occi
           it 'passes `options` to renderer'
         end
 
-        describe '::included'
-        describe '::extended'
-        describe '::available_formats'
-        describe '::available_renderers'
-        describe '::renderer?'
-        describe '::renderer_candidates'
-        describe '::required_renderer_methods'
-        describe '::renderer_namespace'
+        describe '::included' do
+          context 'for every <format>' do
+            it 'adds to_<format> method'
+            it 'overrides existing to_<format> method'
+            it 'redirects to #render from to_<format>'
+          end
+        end
+
+        describe '::extended' do
+          it 'raises exception when passed Class'
+          it 'executes ::included when passed instance'
+        end
+
+        describe '::available_formats' do
+          it 'returns enumerable object'
+          it 'contains String-like items'
+          it 'contains method-compliant names'
+        end
+
+        describe '::available_renderers' do
+          it 'returns a map from format to renderer'
+          it 'returns a map with String-like keys and Class-like values'
+        end
+
+        describe '::renderer?' do
+          it 'returns false for non-Class arguments'
+          it 'returns false for classes not responding to required methods'
+          it 'returns false for classes claiming not to be renderer'
+          it 'returns true for classes claiming to be renderer'
+        end
+
+        describe '::renderer_candidates' do
+          it 'lists candidates from given namespace'
+          it 'returns empty list for empty namespace'
+        end
+
+        describe '::required_renderer_methods' do
+          it 'returns default list of required symbols'
+        end
+
+        describe '::renderer_namespace' do
+          it 'returns default namespace module'
+        end
       end
     end
   end
