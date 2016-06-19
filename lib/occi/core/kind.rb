636836457123ca3b589d04fd84a76d8ff8b4a70e
Boris Parak <parak@cesnet.cz>
2016-06-19 19:55:06 +0200

    [Next] Added `Kind` + specs

--- lib/occi/core/kind.rb
+++ lib/occi/core/kind.rb
@@ -1,6 +1,118 @@
 module Occi
   module Core
+    # Defines the classification system of the OCCI Core Model. The `Kind`
+    # instance represents the type identification mechanism for all `Entity`
+    # instances present in the model.
     #
-    class Kind < Category; end
+    # @example
+    #   Kind.new schema: 'http://schemas.ogf.org/occi/infrastructure#',
+    #            term: 'compute',
+    #            title: 'Compute'
+    #
+    # @attr parent [Kind] previous `Kind` in the OCCI kind hierarchy
+    # @attr actions [Array] list of `Action` instances applicable to this `Kind`
+    # @attr location [URI] protocol agnostic location of this `Kind` instance
+    #
+    # @author Boris Parak <parak@cesnet.cz>
+    class Kind < Category
+      attr_accessor :parent, :actions, :location
+
+      # Checks whether the given `Kind` instance is related
+      # to this instance. The given `Kind` instance must be
+      # included in the list of predecessors (see `#related`)
+      # to succeed.
+      #
+      # @param kind [Kind] suspected predecessor
+      # @return [TrueClass, FalseClass] result
+      def related?(kind)
+        return false unless kind
+        related.include? kind
+      end
+
+      # Checks whether the given `Kind` instance is related
+      # to this instance. The given `Kind` instance must be
+      # the immediate predecessor (see `#directly_related`)
+      # to succeed.
+      #
+      # @param kind [Kind] suspected predecessor
+      # @return [TrueClass, FalseClass] result
+      def directly_related?(kind)
+        return false unless kind
+        directly_related.include? kind
+      end
+
+      # Transitively returns all predecessors of this `Kind` instance in
+      # a multi-element `Array`.
+      #
+      # @return [Array] list containing predecessors of this `Kind` instance
+      def related
+        return directly_related if hierarchy_root?
+        [parent, parent.related].flatten.compact
+      end
+
+      # For compatibility reasons, returns the parent instance of this `Kind` in
+      # a single-element `Array`.
+      #
+      # @return [Array] a single-element list containing the parent `Kind` instance
+      def directly_related
+        [parent].compact
+      end
+
+      # Indicates whether this instance is the base of the OCCI kind
+      # hierarchy, i.e. there are no predecessors. This helps to
+      # calculate the relationship status correctly, see `#related`.
+      #
+      # @return [TrueClass, FalseClass] result
+      def hierarchy_root?
+        parent.nil?
+      end
+
+      protected
+
+      # :nodoc:
+      def defaults
+        category_defaults = super
+
+        category_defaults[:parent] = nil
+        category_defaults[:actions] = []
+        category_defaults[:location] = nil
+
+        category_defaults
+      end
+
+      # :nodoc:
+      def sufficient_args!(args)
+        super
+        [:actions].each do |attr|
+          raise Occi::Core::Errors::MandatoryArgumentError, "#{attr} is a mandatory " \
+                "argument for #{self.class}" if args[attr].nil?
+        end
+      end
+
+      # :nodoc:
+      def post_initialize(args)
+        @parent = args.fetch(:parent)
+        @actions = args.fetch(:actions)
+        @location = args.fetch(:location)
+
+        self.location ||= generate_location
+      end
+
+      private
+
+      # Generates default location based on the already configured
+      # `term` attribute. Fails if `term` is not present.
+      #
+      # @example
+      #   kind.term              # => 'compute'
+      #   kind.generate_location # => '/compute/'
+      #
+      # @return [String] generated location string
+      def generate_location
+        raise Occi::Core::Errors::MandatoryArgumentError,
+              'Cannot generate default location without a `term`' if term.blank?
+        "/#{term}/"
+      end
+    end
   end
 end
---
+++ spec/occi/core/kind_spec.rb
@@ -0,0 +1,131 @@
+module Occi
+  module Core
+    describe Kind do
+      subject { kind }
+
+      let(:example_term) { 'kind' }
+      let(:example_schema) { 'http://schemas.org/schema#' }
+      let(:example_title) { 'Generic kind' }
+
+      let(:root_kind) do
+        Kind.new(
+          term: 'root',
+          schema: 'http://test.org/root#',
+          title: 'Root kind'
+        )
+      end
+
+      let(:kind) do
+        Kind.new(
+          term: example_term,
+          schema: example_schema,
+          title: example_title,
+          parent: root_kind
+        )
+      end
+
+      let(:successor_kind) do
+        Kind.new(
+          term: 'succ',
+          schema: 'http://test.org/succ#',
+          title: 'Succ kind',
+          parent: kind
+        )
+      end
+
+      KIND_ATTRS = [:parent, :location, :actions].freeze
+
+      KIND_ATTRS.each do |attr|
+        it "has #{attr} accessor" do
+          is_expected.to have_attr_accessor attr.to_sym
+        end
+      end
+
+      describe 'related?' do
+        it 'returns `false` without `kind`' do
+          expect(subject.related?(nil)).to be false
+        end
+
+        it 'returns `false` for not related' do
+          expect(subject.related?('wat?')).to be false
+        end
+
+        it 'returns `false` for successor kind' do
+          expect(subject.related?(successor_kind)).to be false
+        end
+
+        it 'returns `true` for related' do
+          expect(subject.related?(root_kind)).to be true
+        end
+      end
+
+      describe 'directly_related?' do
+        subject { successor_kind }
+
+        it 'returns `false` without `kind`' do
+          expect(subject.directly_related?(nil)).to be false
+        end
+
+        it 'returns `false` for not related' do
+          expect(subject.directly_related?('wat?')).to be false
+        end
+
+        it 'returns `false` for not directly related' do
+          expect(subject.directly_related?(root_kind)).to be false
+        end
+
+        it 'returns `true` for directly related' do
+          expect(subject.directly_related?(kind)).to be true
+        end
+      end
+
+      describe 'related' do
+        subject { root_kind.related }
+
+        it 'returns enumerable list' do
+          expect(subject).to be_kind_of(Enumerable)
+        end
+
+        it 'returns empty list for root' do
+          expect(subject).to be_empty
+        end
+
+        it 'returns non-empty list for non-root kind' do
+          expect(kind.related.count).to eq 1
+        end
+      end
+
+      describe 'directly_related' do
+        subject { root_kind.directly_related }
+
+        it 'returns enumerable list' do
+          expect(subject).to be_kind_of(Enumerable)
+        end
+
+        it 'returns empty list for root' do
+          expect(subject).to be_empty
+        end
+
+        it 'returns single-element list for single-predecessor kind' do
+          expect(kind.directly_related.count).to eq 1
+          expect(kind.directly_related).to include(root_kind)
+        end
+
+        it 'returns single-element list for multi-predecessor kind' do
+          expect(successor_kind.directly_related.count).to eq 1
+          expect(successor_kind.directly_related).to include(kind)
+        end
+      end
+
+      describe 'hierarchy_root?' do
+        it 'returns `false` for non-root kind' do
+          expect(root_kind.hierarchy_root?).to be true
+        end
+
+        it 'returns `true` for root kind' do
+          expect(successor_kind.hierarchy_root?).to be false
+        end
+      end
+    end
+  end
+end
