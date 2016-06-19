a75359e9e5470fddfbdc2864af21dacd2af233ee
Boris Parak <parak@cesnet.cz>
2016-06-19 17:57:27 +0200

    [Next] Added `Action` + spec

--- lib/occi/core/action.rb
+++ lib/occi/core/action.rb
@@ -1,6 +1,9 @@
 module Occi
   module Core
+    # Class without internal logic semantically separating `Category` and
+    # `Action` instances for rendering purposes.
     #
+    # @author Boris Parak <parak@cesnet.cz>
     class Action < Category; end
   end
 end
---
+++ spec/occi/core/action_spec.rb
@@ -0,0 +1,20 @@
+module Occi
+  module Core
+    describe Action do
+      subject { Action }
+
+      let(:example_term) { 'action' }
+      let(:example_schema) { 'http://schemas.org/schema#' }
+      let(:example_title) { 'Generic action' }
+      let(:example_attributes) { instance_double('Hash') }
+
+      it 'does not do anything special' do
+        expect do
+          subject.new(
+            schema: example_schema, term: example_term, title: example_title, attributes: example_attributes
+          )
+        end.not_to raise_error
+      end
+    end
+  end
+end
