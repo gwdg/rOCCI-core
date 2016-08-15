4175d30692e7909c323a287db62bd04a324d4b98
Boris Parak <parak@cesnet.cz>
2016-08-15 19:49:21 +0200

    [Next] Reworked IAR spec skeleton

--- spec/occi/core/helpers/instance_attribute_resetter_spec.rb
+++ spec/occi/core/helpers/instance_attribute_resetter_spec.rb
@@ -2,22 +2,52 @@ module Occi
   module Core
     module Helpers
       describe InstanceAttributeResetter do
-        subject { obj_w_attrs }
+        subject { pristine_obj }
 
-        let(:example_attribute) { 'org.example.attribute' }
-        let(:example_value) { 'text' }
-        let(:obj_w_attrs) do
-          object = RocciCoreSpec::ClassWAttributes.clone.new
-          object.attributes = instance_double('Hash')
-          object.extend(InstanceAttributeResetter)
-          object.class.send(:define_method, 'base_attributes', proc { {} })
-          object.class.send(:define_method, 'added_attributes', proc { [] })
-          object
-        end
         let(:pristine_obj) do
           object = Object.clone.new
           object.extend(InstanceAttributeResetter)
           object
+        end
+
+        let(:base_attribute_name) { 'occi.test.attribute' }
+        let(:added_attribute_name) { 'occi.test.added' }
+
+        let(:base_attributes) do
+          { base_attribute_name => instance_double('Occi::Core::AttributeDefinition') }
+        end
+
+        let(:added_attributes) do
+          [{
+            added_attribute_name => instance_double('Occi::Core::AttributeDefinition')
+          }]
+        end
+
+        before(:example) do
+          allow(subject).to receive(:base_attributes).and_return(base_attributes)
+          allow(subject).to receive(:added_attributes).and_return(added_attributes)
+        end
+
+        describe '#reset_attributes!'
+        describe '#reset_base_attributes!'
+        describe '#reset_added_attributes!'
+        describe '#reset_attributes'
+        describe '#remove_undef_attributes'
+
+        describe '#attribute_names' do
+          context 'with attributes'
+          context 'without attributes'
+          context 'with nil attribute names'
+        end
+
+        describe '#reset_base_attributes'
+        describe '#reset_added_attributes'
+
+        describe '#reset_attribute' do
+          context 'when attribute exists'
+          context 'when attribute does not exist'
+          context 'when `force` is used'
+          context 'when `force` is not used'
         end
       end
     end
