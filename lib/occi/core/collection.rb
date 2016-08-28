ad83d5ecae6bc03c9f2ab4b9b59579e3327bf208
Boris Parak <parak@cesnet.cz>
2016-08-28 17:37:41 +0200

    [Next] Added Collection + specs

--- lib/occi/core/collection.rb
+++ lib/occi/core/collection.rb
@@ -19,6 +19,17 @@ module Occi
 
       attr_accessor(*ALL_KEYS)
 
+      # Constructs an instance with the given information. All arguments are
+      # optional and will default to empty `Set` instances if not provided.
+      #
+      # @example
+      #   my_coll = Occi::Core::Collection.new
+      #   my_coll << mixin << kind << entity
+      #
+      # @param args [Hash] arguments with collection instance information
+      # @option args [Set] :categories set of categories associated with this collection instance
+      # @option args [Set] :entities set of entities associated with this collection instance
+      # @option args [Set] :action_instances set of action instances associated with this collection instance
       def initialize(args = {})
         pre_initialize(args)
         default_args! args
@@ -28,30 +39,76 @@ module Occi
         post_initialize(args)
       end
 
+      # Collects everything present in this collection and merges it into
+      # a single set. This will include categories, entities, and action instances.
+      # The resulting set can be used, for example, in conjunction with the `<<`
+      # operator to create an independent copy of the collection.
+      #
+      # @return [Set] content of this collection merged into a single set
       def all
         Set.new(ALL_KEYS.collect { |key| send(key).to_a }.flatten.compact)
       end
 
+      # Collects all `Occi::Core::Kind` instances in this collection.
+      #
+      # @return [Set] all `Occi::Core::Kind` instances from this collection
       def kinds
         typed_set(categories, Occi::Core::Kind)
       end
 
+      # Collects all `Occi::Core::Mixin` instances in this collection.
+      #
+      # @return [Set] all `Occi::Core::Mixin` instances from this collection
       def mixins
         typed_set(categories, Occi::Core::Mixin)
       end
 
+      # Collects all `Occi::Core::Action` instances in this collection.
+      #
+      # @return [Set] all `Occi::Core::Action` instances from this collection
       def actions
         typed_set(categories, Occi::Core::Action)
       end
 
+      # Collects all `Occi::Core::Resource` instances in this collection.
+      #
+      # @return [Set] all `Occi::Core::Resource` instances from this collection
       def resources
         typed_set(entities, Occi::Core::Resource)
       end
 
+      # Collects all `Occi::Core::Link` instances in this collection.
+      #
+      # @return [Set] all `Occi::Core::Link` instances from this collection
       def links
         typed_set(entities, Occi::Core::Link)
       end
 
+      # Collects all `Occi::Core::Kind` instances related to the given instance.
+      #
+      # @param kind [Occi::Core::Kind] top-level kind
+      # @param options [Hash] look-up modifiers, currently only `directly: true`
+      # @return [Set] all instances related to the given instance
+      def find_related(kind, options = { directly: false })
+        raise ArgumentError, 'Kind is a mandatory argument' unless kind
+        method = options[:directly] ? :directly_related? : :related?
+        Set.new(kinds.select { |knd| knd.send(method, kind) })
+      end
+
+      # Collects all `Occi::Core::Mixin` instances dependent on the given instance.
+      #
+      # @param mixin [Occi::Core::Mixin] top-level mixin
+      # @return [Set] all instances dependent on the given instance
+      def find_dependent(mixin)
+        raise ArgumentError, 'Mixin is a mandatory argument' unless mixin
+        Set.new(mixins.select { |mxn| mxn.depends?(mixin) })
+      end
+
+      # Collects all categories and entities with the given location.
+      # This method looks for an explicit/full match on the location.
+      #
+      # @param location [URI] expected location
+      # @return [Set] set of results possibly containing a mix of instance types
       def find_by_location(location)
         filtered_set(
           all.select { |elm| elm.respond_to?(:location) },
@@ -59,32 +116,71 @@ module Occi
         )
       end
 
+      # Collects all `Occi::Core::Entity` successors with the given
+      # kind. The resulting set may contain mixed instance types.
+      #
+      # @param kind [Occi::Core::Kind] expected kind
+      # @return [Set] set of entities with the given kind
       def find_by_kind(kind)
-        raise ArgumentError, 'Kind must be an Occi::Core::Kind instance' unless kind.is_a?(Occi::Core::Kind)
+        raise ArgumentError, 'Kind is a mandatory argument' unless kind
         filtered_set(entities, key: 'kind', value: kind)
       end
 
+      # Collects all `Occi::Core::ActionInstance` instances with the given action.
+      #
+      # @param action [Occi::Core::Action] expected action
+      # @return [Set] set of found action instances
       def find_by_action(action)
-        raise ArgumentError, 'Action must be an Occi::Core::Action instance' unless action.is_a?(Occi::Core::Action)
+        raise ArgumentError, 'Action is a mandatory argument' unless action
         filtered_set(action_instances, key: 'action', value: action)
       end
 
+      # Collects all `Occi::Core::Entity` successors associated with the given mixin.
+      #
+      # @param mixin [Occi::Core::Mixin] expected mixin
+      # @return [Set] set of found entities
+      def find_by_mixin(mixin)
+        raise ArgumentError, 'Mixin is a mandatory argument' unless mixin
+        Set.new(entities.select { |elm| elm.mixins.include?(mixin) })
+      end
+
+      # Collects all `Occi::Core::Entity` successors with the given ID.
+      #
+      # @param id [String] expected ID
+      # @return [Set] set of found entities
       def find_by_id(id)
         filtered_set(entities, key: 'id', value: id)
       end
 
+      # Collects all `Occi::Core::Category` successors with the given identifier.
+      #
+      # @param identifier [String] expected identifier
+      # @return [Set] set of found categories
       def find_by_identifier(identifier)
         filtered_set(categories, key: 'identifier', value: identifier)
       end
 
+      # Collects all `Occi::Core::Category` successors with the given term.
+      #
+      # @param term [String] expected term
+      # @return [Set] set of found categories
       def find_by_term(term)
         filtered_set(categories, key: 'term', value: term)
       end
 
+      # Collects all `Occi::Core::Category` successors with the given schema.
+      #
+      # @param schema [String] expected schema
+      # @return [Set] set of found categories
       def find_by_schema(schema)
         filtered_set(categories, key: 'schema', value: schema)
       end
 
+      # Auto-assigns the given object to the appropriate internal set.
+      # Unknown objects will result in an `ArgumentError` error.
+      #
+      # @param object [Object] object to be assigned
+      # @return [Occi::Core::Collection] self, for chaining purposes
       def <<(object)
         case object
         when Occi::Core::Category
@@ -101,6 +197,11 @@ module Occi
       end
       alias add <<
 
+      # Auto-removes the given object from the appropriate internal set.
+      # Unknown objects will result in an `ArgumentError` error.
+      #
+      # @param object [Object] object to be removed
+      # @return [Occi::Core::Collection] self, for chaining purposes
       def remove(object)
         case object
         when Occi::Core::Category
@@ -116,10 +217,20 @@ module Occi
         self
       end
 
+      # Validates entities and action instances stored in this collection.
+      # Validity of each instance is considered independently. If you are
+      # looking for a more aggressive version raising validation errors,
+      # see `#valid!`.
+      #
+      # @return [TrueClass] on successful validation
+      # @return [FalseClass] on failed validation
       def valid?
         entities.collect(&:valid?).reduce(true, :&) && action_instances.collect(&:valid?).reduce(true, :&)
       end
 
+      # Validates entities and action instances stored in this collection.
+      # Validity of each instance is considered independently. This method
+      # will raise an error on the first invalid instance.
       def valid!
         entities.each(&:valid!)
         action_instances.each(&:valid!)
@@ -150,13 +261,15 @@ module Occi
 
       private
 
+      # :nodoc:
       def typed_set(source, type)
-        Set.new(source.collect { |elm| elm.is_a?(type) })
+        Set.new(source.select { |elm| elm.is_a?(type) })
       end
 
+      # :nodoc:
       def filtered_set(source, filter)
         raise ArgumentError, 'Filtering key is a mandatory argument' if filter[:key].blank?
-        Set.new(source.collect { |elm| elm.send(filter[:key]) == filter[:value] })
+        Set.new(source.select { |elm| elm.send(filter[:key]) == filter[:value] })
       end
     end
   end
--- spec/occi/core/collection_spec.rb
+++ spec/occi/core/collection_spec.rb
@@ -4,6 +4,18 @@ module Occi
       subject(:coll) { collection }
       let(:collection) { Collection.new }
 
+      let(:kind) { instance_double('Occi::Core::Kind') }
+      let(:mixin) { instance_double('Occi::Core::Mixin') }
+      let(:action) { instance_double('Occi::Core::Action') }
+      let(:resource) { instance_double('Occi::Core::Resource') }
+      let(:link) { instance_double('Occi::Core::Link') }
+      let(:action_instance) { instance_double('Occi::Core::ActionInstance') }
+
+      let(:all) { Set.new([kind, mixin, action, resource, link, action_instance]) }
+      let(:categories) { Set.new([kind, mixin, action]) }
+      let(:entities) { Set.new([resource, link]) }
+      let(:action_instances) { Set.new([action_instance]) }
+
       COLL_ATTRS = [:categories, :entities, :action_instances].freeze
 
       COLL_ATTRS.each do |attr|
@@ -15,6 +27,381 @@ module Occi
       it 'has logger' do
         expect(coll).to respond_to(:logger)
         expect(coll.class).to respond_to(:logger)
+      end
+
+      before(:example) do
+        coll.categories = categories
+        coll.entities = entities
+        coll.action_instances = action_instances
+
+        [:kind, :mixin, :action, :resource, :link].each do |elm|
+          allow(send(elm)).to receive(:is_a?).with(Class).and_return(false)
+          allow(send(elm)).to receive(:is_a?).with(Occi::Core.const_get(elm.to_s.capitalize)).and_return(true)
+        end
+      end
+
+      describe '#all' do
+        it 'returns everything' do
+          expect(coll.all).to eq all
+        end
+      end
+
+      describe '#kinds' do
+        let(:kinds) { Set.new([kind]) }
+
+        it 'returns only kinds' do
+          expect(coll.kinds).to eq kinds
+        end
+      end
+
+      describe '#mixins' do
+        let(:mixins) { Set.new([mixin]) }
+
+        it 'returns only mixins' do
+          expect(coll.mixins).to eq mixins
+        end
+      end
+
+      describe '#actions' do
+        let(:actions) { Set.new([action]) }
+
+        it 'returns only actions' do
+          expect(coll.actions).to eq actions
+        end
+      end
+
+      describe '#resources' do
+        let(:resources) { Set.new([resource]) }
+
+        it 'returns only resources' do
+          expect(coll.resources).to eq resources
+        end
+      end
+
+      describe '#links' do
+        let(:links) { Set.new([link]) }
+
+        it 'returns only links' do
+          expect(coll.links).to eq links
+        end
+      end
+
+      describe '#find_related' do
+        context 'without kind' do
+          it 'raises error' do
+            expect { coll.find_related(nil) }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'with kind' do
+          let(:sample_kind) { instance_double('Occi::Core::Kind') }
+
+          it 'returns set of kinds by default' do
+            expect(kind).to receive(:related?).with(sample_kind).and_return(true)
+            expect(coll.find_related(sample_kind)).to eq Set.new([kind])
+          end
+
+          it 'returns set of directly related kinds' do
+            expect(kind).to receive(:directly_related?).with(sample_kind).and_return(true)
+            expect(coll.find_related(sample_kind, directly: true)).to eq Set.new([kind])
+          end
+        end
+      end
+
+      describe '#find_dependent' do
+        context 'without mixin' do
+          it 'raises error' do
+            expect { coll.find_dependent(nil) }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'with mixin' do
+          let(:sample_mixin) { instance_double('Occi::Core::Mixin') }
+
+          it 'returns set of mixins' do
+            expect(mixin).to receive(:depends?).with(sample_mixin).and_return(true)
+            expect(coll.find_dependent(sample_mixin)).to eq Set.new([mixin])
+          end
+        end
+      end
+
+      describe '#find_by_location' do
+        before(:example) do
+          [:kind, :mixin, :resource, :link].each do |elm|
+            allow(send(elm)).to receive(:location).and_return(fake_location)
+          end
+        end
+
+        let(:fake_location) { URI.parse('/fake/location/') }
+        let(:locatable) { Set.new([kind, mixin, resource, link]) }
+
+        it 'returns set of instances with matching location' do
+          expect(coll.find_by_location(fake_location)).to eq locatable
+        end
+      end
+
+      describe '#find_by_kind' do
+        context 'without kind' do
+          it 'raises error' do
+            expect { coll.find_by_kind(nil) }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'with kind' do
+          let(:sample_kind) { instance_double('Occi::Core::Kind') }
+
+          it 'returns set of entities with the given kind' do
+            expect(resource).to receive(:kind).and_return(sample_kind)
+            expect(link).to receive(:kind).and_return(sample_kind)
+            expect(coll.find_by_kind(sample_kind)).to eq Set.new([resource, link])
+          end
+        end
+      end
+
+      describe '#find_by_action' do
+        context 'without action' do
+          it 'raises error' do
+            expect { coll.find_by_action(nil) }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'with action' do
+          let(:sample_action) { instance_double('Occi::Core::Action') }
+
+          it 'returns set of AIs with the given action' do
+            expect(action_instance).to receive(:action).and_return(sample_action)
+            expect(coll.find_by_action(sample_action)).to eq action_instances
+          end
+        end
+      end
+
+      describe '#find_by_mixin' do
+        context 'without mixin' do
+          it 'raises error' do
+            expect { coll.find_by_mixin(nil) }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'with mixin' do
+          let(:sample_mixin) { instance_double('Occi::Core::Mixin') }
+
+          it 'returns set of entities with the given mixin' do
+            expect(resource).to receive(:mixins).and_return(Set.new([sample_mixin]))
+            expect(link).to receive(:mixins).and_return(Set.new([sample_mixin]))
+            expect(coll.find_by_mixin(sample_mixin)).to eq Set.new([resource, link])
+          end
+        end
+      end
+
+      describe '#find_by_id' do
+        let(:sample_id) { '64654f-6a5df4a6-df46ad4f' }
+
+        it 'returns set of entities with the given occi.core.id' do
+          expect(resource).to receive(:id).and_return(sample_id)
+          expect(link).to receive(:id).and_return(sample_id)
+          expect(coll.find_by_id(sample_id)).to eq Set.new([resource, link])
+        end
+      end
+
+      describe '#find_by_identifier' do
+        let(:sample_identifier) { 'http://my.schema.com/test#shutup' }
+
+        it 'returns set of categories with the given identifier' do
+          categories.each { |cat| expect(cat).to receive(:identifier).and_return(sample_identifier) }
+          expect(coll.find_by_identifier(sample_identifier)).to eq categories
+        end
+      end
+
+      describe '#find_by_term' do
+        let(:sample_term) { 'shutup' }
+
+        it 'returns set of categories with the given term' do
+          categories.each { |cat| expect(cat).to receive(:term).and_return(sample_term) }
+          expect(coll.find_by_term(sample_term)).to eq categories
+        end
+      end
+
+      describe '#find_by_schema' do
+        let(:sample_schema) { 'http://my.schema.com/test#' }
+
+        it 'returns set of categories with the given schema' do
+          categories.each { |cat| expect(cat).to receive(:schema).and_return(sample_schema) }
+          expect(coll.find_by_schema(sample_schema)).to eq categories
+        end
+      end
+
+      describe '#<<' do
+        context 'with category' do
+          before(:example) do
+            coll.categories = Set.new
+          end
+
+          let(:kind) { Occi::Core::Kind.new(term: 'kind', schema: 'http://my.test.com/new#') }
+          let(:mixin) { Occi::Core::Mixin.new(term: 'mixin', schema: 'http://my.test.com/new#') }
+          let(:action) { Occi::Core::Action.new(term: 'action', schema: 'http://my.test.com/new#') }
+          let(:categories) { Set.new([kind, mixin, action]) }
+
+          it 'assigns to categories' do
+            expect(coll.categories).to be_empty
+            expect { coll << kind }.not_to raise_error
+            expect { coll << mixin }.not_to raise_error
+            expect { coll << action }.not_to raise_error
+            expect(coll.categories).to eq categories
+          end
+        end
+
+        context 'with entity' do
+          before(:example) do
+            coll.entities = Set.new
+          end
+
+          let(:kind) { Occi::Core::Kind.new(term: 'kind', schema: 'http://my.test.com/new#') }
+          let(:resource) { Occi::Core::Resource.new(kind: kind, title: 'My Resource') }
+          let(:link) { Occi::Core::Link.new(kind: kind, title: 'My Link') }
+          let(:entities) { Set.new([resource, link]) }
+
+          it 'assigns to entities' do
+            expect(coll.entities).to be_empty
+            expect { coll << resource }.not_to raise_error
+            expect { coll << link }.not_to raise_error
+            expect(coll.entities).to eq entities
+          end
+        end
+
+        context 'with action instance' do
+          before(:example) do
+            coll.action_instances = Set.new
+          end
+
+          let(:action) { Occi::Core::Action.new(term: 'action', schema: 'http://my.test.com/new#') }
+          let(:action_instance) { Occi::Core::ActionInstance.new(action: action) }
+          let(:action_instances) { Set.new([action_instance]) }
+
+          it 'assigns to action_instances' do
+            expect(coll.action_instances).to be_empty
+            expect { coll << action_instance }.not_to raise_error
+            expect(coll.action_instances).to eq action_instances
+          end
+        end
+
+        context 'with unknown object' do
+          it 'raises error' do
+            expect { coll << Object.new }.to raise_error(ArgumentError)
+          end
+        end
+      end
+
+      describe '#remove' do
+        context 'with category' do
+          before(:example) do
+            coll.categories = categories
+          end
+
+          let(:kind) { Occi::Core::Kind.new(term: 'kind', schema: 'http://my.test.com/new#') }
+          let(:mixin) { Occi::Core::Mixin.new(term: 'mixin', schema: 'http://my.test.com/new#') }
+          let(:action) { Occi::Core::Action.new(term: 'action', schema: 'http://my.test.com/new#') }
+          let(:categories) { Set.new([kind, mixin, action]) }
+
+          it 'assigns to categories' do
+            expect(coll.categories).to eq categories
+            expect { coll.remove(kind) }.not_to raise_error
+            expect { coll.remove(mixin) }.not_to raise_error
+            expect { coll.remove(action) }.not_to raise_error
+            expect(coll.categories).to be_empty
+          end
+        end
+
+        context 'with entity' do
+          before(:example) do
+            coll.entities = entities
+          end
+
+          let(:kind) { Occi::Core::Kind.new(term: 'kind', schema: 'http://my.test.com/new#') }
+          let(:resource) { Occi::Core::Resource.new(kind: kind, title: 'My Resource') }
+          let(:link) { Occi::Core::Link.new(kind: kind, title: 'My Link') }
+          let(:entities) { Set.new([resource, link]) }
+
+          it 'assigns to entities' do
+            expect(coll.entities).to eq entities
+            expect { coll.remove(resource) }.not_to raise_error
+            expect { coll.remove(link) }.not_to raise_error
+            expect(coll.entities).to be_empty
+          end
+        end
+
+        context 'with action instance' do
+          before(:example) do
+            coll.action_instances = action_instances
+          end
+
+          let(:action) { Occi::Core::Action.new(term: 'action', schema: 'http://my.test.com/new#') }
+          let(:action_instance) { Occi::Core::ActionInstance.new(action: action) }
+          let(:action_instances) { Set.new([action_instance]) }
+
+          it 'assigns to action_instances' do
+            expect(coll.action_instances).to eq action_instances
+            expect { coll.remove(action_instance) }.not_to raise_error
+            expect(coll.action_instances).to be_empty
+          end
+        end
+
+        context 'with unknown object' do
+          it 'raises error' do
+            expect { coll.remove(Object.new) }.to raise_error(ArgumentError)
+          end
+        end
+      end
+
+      describe '#valid?' do
+        context 'with valid instances' do
+          before(:example) do
+            expect(resource).to receive(:valid?).and_return(true)
+            expect(link).to receive(:valid?).and_return(true)
+            expect(action_instance).to receive(:valid?).and_return(true)
+          end
+
+          it 'calls `valid?` on entities and action instances' do
+            expect(coll.valid?).to be true
+          end
+        end
+
+        context 'with invalid instances' do
+          before(:example) do
+            allow(resource).to receive(:valid?).and_return(false)
+            allow(link).to receive(:valid?).and_return(false)
+            allow(action_instance).to receive(:valid?).and_return(false)
+          end
+
+          it 'calls `valid?` on entities and action instances' do
+            expect(coll.valid?).to be false
+          end
+        end
+      end
+
+      describe '#valid!' do
+        context 'with valid instances' do
+          before(:example) do
+            expect(resource).to receive(:valid!)
+            expect(link).to receive(:valid!)
+            expect(action_instance).to receive(:valid!)
+          end
+
+          it 'calls `valid!` on entities and action instances' do
+            expect { coll.valid! }.not_to raise_error
+          end
+        end
+
+        context 'with invalid instances' do
+          before(:example) do
+            allow(resource).to receive(:valid!).and_raise(Occi::Core::Errors::ValidationError)
+            allow(link).to receive(:valid!).and_raise(Occi::Core::Errors::ValidationError)
+            allow(action_instance).to receive(:valid!).and_raise(Occi::Core::Errors::ValidationError)
+          end
+
+          it 'calls `valid!` on entities and action instances' do
+            expect { coll.valid! }.to raise_error(Occi::Core::Errors::ValidationError)
+          end
+        end
       end
     end
   end
