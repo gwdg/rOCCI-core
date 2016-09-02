eff676727fdd33f5939e6a59160e2509b57d589d
Boris Parak <parak@cesnet.cz>
2016-09-02 21:51:46 +0200

    [Next] Text serializer draft, WIP (no specs)

--- lib/occi/core/renderers/text/base.rb
+++ lib/occi/core/renderers/text/base.rb
@@ -3,13 +3,29 @@ module Occi
     module Renderers
       module Text
         class Base
-          class <<self
-            # Renders the given object to `text`.
-            #
-            # @param object [Object] instance to be rendered
-            # @param options [Hash] additional options
-            # @return [String] object rendering
-            def render(object, options); end
+          # Ruby 2.3 compatibility, with `$SAFE` changes
+          RENDER_SAFE = (RUBY_VERSION >= '2.3') ? 1 : 3
+
+          # Renders the given object to `text`.
+          #
+          # @param object [Object] instance to be rendered
+          # @param options [Hash] additional options
+          # @return [String] object rendering as plain text
+          # @return [Hash] object rendering as hash
+          def self.render(object, options)
+            case options[:format]
+            when 'text', 'text_plain'
+              render_plain object, options
+            when 'headers', 'text_occi'
+              render_headers object, options
+            else
+              raise "Rendering to #{options[:format]} is not supported"
+            end
+          end
+
+          # @return [Integer] SAFE level
+          def self.render_safe
+            RENDER_SAFE
           end
         end
       end
--- lib/occi/core/renderers/text/category.rb
+++ lib/occi/core/renderers/text/category.rb
@@ -4,7 +4,72 @@ module Occi
   module Core
     module Renderers
       module Text
-        class Category < Base; end
+        class Category < Base
+          class << self
+            # @return [String] textual representation of Category
+            def render_plain(object, _options)
+              obj_data = object_data(object)
+              "Category: #{ERB.new(template, render_safe).result(binding)}"
+            end
+
+            # @return [Hash] hash-like textual representation of Category
+            def render_headers(object, _options)
+              obj_data = object_data(object)
+              { 'X-OCCI-Category' => [ERB.new(template, render_safe).result(binding)] }
+            end
+
+            def object_data(object)
+              {
+                term: object.term, schema: object.schema,
+                subclass: render_subclass(object), title: object.title,
+                rel: render_parent(object),
+                location: render_location(object),
+                attributes: render_attributes(object),
+                actions: render_actions(object)
+              }
+            end
+
+            def render_parent(object)
+              cand = if object.respond_to?(:directly_related)
+                       object.directly_related.first
+                     elsif object.respond_to?(:depends)
+                       object.depends.first
+                     end
+              cand ? cand.identifier : nil
+            end
+
+            def render_subclass(object)
+              object.class.name.demodulize.downcase
+            end
+
+            def render_location(object)
+              object.respond_to?(:location) ? object.location : nil
+            end
+
+            def render_attributes(object)
+              return unless object.respond_to?(:attributes)
+              attrs = object.attributes.keys
+              attrs.empty? ? nil : attrs.join(' ')
+            end
+
+            def render_actions(object)
+              return unless object.respond_to?(:actions)
+              acts = object.actions.collect(&:identifier)
+              acts.empty? ? nil : acts.join(' ')
+            end
+
+            def template
+              '<%= obj_data[:term] %>; ' \
+              'scheme="<%= obj_data[:schema] %>"; ' \
+              'class="<%= obj_data[:subclass] %>"; ' \
+              'title="<%= obj_data[:title] %>"' \
+              '<% if obj_data[:rel] %>; rel="<%= obj_data[:rel] %>"<% end %>' \
+              '<% if obj_data[:location] %>; location="<%= obj_data[:location] %>"<% end %>' \
+              '<% if obj_data[:attributes] %>; attributes="<%= obj_data[:attributes] %>"<% end %>' \
+              '<% if obj_data[:actions] %>; actions="<%= obj_data[:actions] %>"<% end %>'
+            end
+          end
+        end
       end
     end
   end
