598bb7971e1bf66a4d25ae97446ec5d9db6dd154
Boris Parak <parak@cesnet.cz>
2016-09-07 14:25:42 +0200

    [Next] Added specs for TR AI

--- spec/occi/core/renderers/text/action_instance_spec.rb
+++ spec/occi/core/renderers/text/action_instance_spec.rb
@@ -3,6 +3,48 @@ module Occi
     module Renderers
       module Text
         describe ActionInstance do
+          subject(:trai) { ActionInstance.new(ai, options) }
+
+          let(:example_term) { 'action' }
+          let(:example_schema) { 'http://schemas.org/schema#' }
+          let(:example_title) { 'Generic action' }
+          let(:attr_def) { Occi::Core::AttributeDefinition.new }
+          let(:example_attributes) { { 'test' => attr_def } }
+
+          let(:action) do
+            Occi::Core::Action.new(
+              term: example_term, schema: example_schema,
+              title: example_title, attributes: example_attributes
+            )
+          end
+
+          let(:ai) do
+            Occi::Core::ActionInstance.new(action: action)
+          end
+
+          context 'with text format' do
+            let(:options) { { format: 'text' } }
+
+            it 'renders with action' do
+              expect(trai.render).to include('Category: ')
+            end
+
+            it 'renders with attributes' do
+              expect(trai.render).to include('X-OCCI-Attribute: ')
+            end
+          end
+
+          context 'with headers format' do
+            let(:options) { { format: 'headers' } }
+
+            it 'renders with action' do
+              expect(trai.render).to include('X-OCCI-Category')
+            end
+
+            it 'renders with attributes' do
+              expect(trai.render).to include('X-OCCI-Attribute')
+            end
+          end
         end
       end
     end
