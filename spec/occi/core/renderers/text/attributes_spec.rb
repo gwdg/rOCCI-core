58ad53cdc205bbe4ce9b17f8b3f5031bf2caff14
Boris Parak <parak@cesnet.cz>
2016-09-07 11:30:12 +0200

    [Next] Added specs for TR Attrs

--- spec/occi/core/renderers/text/attributes_spec.rb
+++ spec/occi/core/renderers/text/attributes_spec.rb
@@ -3,7 +3,17 @@ module Occi
     module Renderers
       module Text
         describe Attributes do
-          subject(:tra) { Attributes.new }
+          subject(:tra) { Attributes.new(nil, nil) }
+
+          let(:attribute_title) { 'occi.core.title' }
+          let(:attribute_id) { 'occi.core.id' }
+
+          let(:kind_attributes) do
+            {
+              attribute_title => instance_double('Occi::Core::AttributeDefinition'),
+              attribute_id    => instance_double('Occi::Core::AttributeDefinition')
+            }
+          end
 
           let(:attr_def_string) do
             Occi::Core::AttributeDefinition.new
@@ -29,30 +39,209 @@ module Occi
             Occi::Core::AttributeDefinition.new(type: Array)
           end
 
-          let(:attribute) do
-            Occi::Core::Attribute.new(
-              value: 'Text',
-              attribute_definition: attr_def_string
-            )
+          let(:attribute_name) { 'test' }
+          let(:attributes) do
+            { attribute_name => Occi::Core::Attribute.new }
+          end
+
+          let(:kind) { Occi::Core::Kind.new(term: 'kind', schema: 'http://sche.ma/tes#', attributes: kind_attributes) }
+
+          before do
+            allow(kind_attributes[attribute_title]).to receive(:default)
+            allow(kind_attributes[attribute_id]).to receive(:default)
           end
 
           describe '#render' do
-            context 'with string attribute' do
+            context 'as plain with string attribute' do
+              let(:value) { 'text' }
+              let(:options) { { format: 'text' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_string
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq "X-OCCI-Attribute: #{attribute_name}=\"#{value}\""
+              end
             end
 
-            context 'with numeric attribute' do
+            context 'as plain with numeric attribute' do
+              let(:value) { 1.0 }
+              let(:options) { { format: 'text' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_numeric
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq "X-OCCI-Attribute: #{attribute_name}=#{value}"
+              end
             end
 
-            context 'with bool attribute' do
+            context 'as plain with bool attribute' do
+              let(:value) { true }
+              let(:options) { { format: 'text' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_bool
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq "X-OCCI-Attribute: #{attribute_name}=#{value}"
+              end
             end
 
-            context 'with category attribute' do
+            context 'as plain with category attribute' do
+              let(:value) { kind }
+              let(:options) { { format: 'text' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_kind
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq "X-OCCI-Attribute: #{attribute_name}=\"#{value.identifier}\""
+              end
             end
 
-            context 'with entity attribute' do
+            context 'as plain with entity attribute' do
+              let(:value) { Occi::Core::Resource.new(kind: kind) }
+              let(:options) { { format: 'text' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_res
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq "X-OCCI-Attribute: #{attribute_name}=\"#{value.location}\""
+              end
             end
 
-            context 'with unknown attribute' do
+            context 'as plain with unknown attribute' do
+              let(:value) { [] }
+              let(:options) { { format: 'text' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_unkn
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'raises error' do
+                expect { tra.render }.to raise_error(Occi::Core::Errors::RenderingError)
+              end
+            end
+
+            context 'as plain with string attribute' do
+              let(:value) { 'text' }
+              let(:options) { { format: 'headers' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_string
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq('X-OCCI-Attribute' => ["#{attribute_name}=\"#{value}\""])
+              end
+            end
+
+            context 'as plain with numeric attribute' do
+              let(:value) { 1.0 }
+              let(:options) { { format: 'headers' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_numeric
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq('X-OCCI-Attribute' => ["#{attribute_name}=#{value}"])
+              end
+            end
+
+            context 'as plain with bool attribute' do
+              let(:value) { true }
+              let(:options) { { format: 'headers' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_bool
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq('X-OCCI-Attribute' => ["#{attribute_name}=#{value}"])
+              end
+            end
+
+            context 'as plain with category attribute' do
+              let(:value) { kind }
+              let(:options) { { format: 'headers' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_kind
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq('X-OCCI-Attribute' => ["#{attribute_name}=\"#{value.identifier}\""])
+              end
+            end
+
+            context 'as plain with entity attribute' do
+              let(:value) { Occi::Core::Resource.new(kind: kind) }
+              let(:options) { { format: 'headers' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_res
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'renders' do
+                expect(tra.render).to eq('X-OCCI-Attribute' => ["#{attribute_name}=\"#{value.location}\""])
+              end
+            end
+
+            context 'as plain with unknown attribute' do
+              let(:value) { [] }
+              let(:options) { { format: 'headers' } }
+
+              before do
+                attributes[attribute_name].value = value
+                attributes[attribute_name].attribute_definition = attr_def_unkn
+                tra.object = attributes
+                tra.options = options
+              end
+
+              it 'raises error' do
+                expect { tra.render }.to raise_error(Occi::Core::Errors::RenderingError)
+              end
             end
           end
         end
