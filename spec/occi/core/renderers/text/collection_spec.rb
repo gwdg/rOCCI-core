579ddfbcc6fcd4581eaf8eb5987cc7cdc6bffb90
Boris Parak <parak@cesnet.cz>
2017-02-26 18:20:54 +0100

    [TR] Added renderer for `Collection`, WIP

--- spec/occi/core/renderers/text/collection_spec.rb
+++ spec/occi/core/renderers/text/collection_spec.rb
@@ -3,7 +3,27 @@ module Occi
     module Renderers
       module Text
         describe Collection do
-          it 'does something'
+          subject(:collr) { collection_renderer }
+
+          let(:empty_collection) { Occi::Core::Collection.new }
+          let(:options) { { format: 'text' } }
+          let(:collection_renderer) { Collection.new(empty_collection, options) }
+
+          BASE_ATTRS = [:object, :options].freeze
+          BASE_ATTRS.each do |attr|
+            it "has #{attr} accessor" do
+              is_expected.to have_attr_accessor attr.to_sym
+            end
+          end
+
+          it 'has logger' do
+            expect(collr).to respond_to(:logger)
+            expect(collr.class).to respond_to(:logger)
+          end
+
+          describe '#render' do
+            it 'does something'
+          end
         end
       end
     end
