e6046be2fa97b3a3d672b8959237945aa8a7fab0
Boris Parak <parak@cesnet.cz>
2017-03-24 14:22:24 +0100

    [Core] Allowing inline attribute defs in YAMLs
    This a temporary workaround and will be removed as soon as name-only
    attribute references in YAMLs are supported.

--- lib/occi/core/action.rb
+++ lib/occi/core/action.rb
@@ -4,6 +4,15 @@ module Occi
     # `Action` instances for rendering purposes.
     #
     # @author Boris Parak <parak@cesnet.cz>
-    class Action < Category; end
+    class Action < Category
+      class << self
+        # :nodoc:
+        def allowed_yaml_classes
+          # TODO: get rid of this with referenced (name-only) attributes in Action YAMLs
+          [String, Regexp, Occi::Core::Resource, IPAddr, Integer, Float].freeze
+        end
+        private :allowed_yaml_classes
+      end
+    end
   end
 end
