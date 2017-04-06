ae7a36f91d9276bf78554da009803fef78ba201a
Boris Parak <parak@cesnet.cz>
2017-04-06 13:30:37 +0200

    [TextParser] Added header transformation logic

---
+++ lib/occi/core/errors/parsing_error.rb
@@ -0,0 +1,11 @@
+module Occi
+  module Core
+    module Errors
+      # Custom error class indicating parsing failures on
+      # various renderings.
+      #
+      # @author Boris Parak <parak@cesnet.cz>
+      class ParsingError < StandardError; end
+    end
+  end
+end
--- lib/occi/core/parsers/text_parser.rb
+++ lib/occi/core/parsers/text_parser.rb
@@ -35,6 +35,17 @@ module Occi
         OCCI_TEXT_TYPES    = [HEADERS_TEXT_TYPES, PLAIN_TEXT_TYPES].flatten.freeze
         MEDIA_TYPES        = [URI_LIST_TYPES, OCCI_TEXT_TYPES].flatten.freeze
 
+        # Constants for OCCI keys
+        KEY_NAMES      = %w[LOCATION_KEYS CATEGORY_KEYS LINK_KEYS ATTRIBUTE_KEYS].freeze
+        LOCATION_KEYS  = %w[X-OCCI-Location X_occi_location X-occi-location Location].freeze
+        CATEGORY_KEYS  = %w[Category X-OCCI-Category X_occi_category X-occi-category].freeze
+        LINK_KEYS      = %w[Link X-OCCI-Link X_occi_link X-occi-link].freeze
+        ATTRIBUTE_KEYS = %w[X-OCCI-Attribute X_occi_attribute X-occi-attribute].freeze
+        OCCI_KEYS      = [LOCATION_KEYS, CATEGORY_KEYS, LINK_KEYS, ATTRIBUTE_KEYS].flatten.freeze
+
+        # Constants for header normalization
+        HEADER_HTTP_PREFIX = 'HTTP_'.freeze
+
         attr_accessor :model, :media_type
 
         # TODO: docs
@@ -117,6 +128,104 @@ module Occi
           # @return [FalseClass] if not supported
           def parses?(media_type)
             media_types.include? media_type
+          end
+
+          # Transforms a `String`-like body into a series of independent lines.
+          # Note: Multi-line elements are not supported by this parser.
+          #
+          # @param body [String] multi-line body
+          # @return [Array] an array of lines
+          def transform_body(body)
+            body.respond_to?(:lines) ? body.lines : body.split("\n")
+          end
+
+          # Transforms a widely varied content of headers into a unified body-like
+          # format for further processing. This method and its helpers should hide
+          # differences between `text/occi` and `text/plain` completely.
+          #
+          # @param headers [Hash] hash with raw header key-value pairs
+          # @return [Array] an array of body-like lines
+          def transform_headers(headers)
+            unify_headers(
+              canonize_headers(
+                normalize_headers(headers)
+              )
+            )
+          end
+
+          # Normalizes raw headers into a more agreeable form. This process will remove known prefixes
+          # as well as blank lines or non-OCCI keys.
+          #
+          # @param headers [Hash] hash with raw header key-value pairs
+          # @return [Hash] a cleaner hash with relevant headers
+          def normalize_headers(headers)
+            headers = Hash[
+              headers.map { |k, v| [k.gsub(HEADER_HTTP_PREFIX, '').capitalize, v] }
+            ]                                                 # remove 'HTTP_' prefix in keys
+            headers.delete_if { |_k, v| v.blank? }            # remove pairs with empty values
+            headers.keep_if { |k, _v| OCCI_KEYS.include?(k) } # drop non-OCCI pairs
+            headers
+          end
+
+          # Canonizes normalized headers by eliminating naming differences in keys. Input for
+          # this method must be already normalized by `normalize_headers`.
+          #
+          # @param headers [Hash] hash with normalized header key-value pairs
+          # @return [Hash] canonized hash with predictable keys
+          def canonize_headers(headers)
+            validate_header_keys! headers.keys
+
+            canonical = {}
+            headers.each_pair do |key, value|
+              pref = key_name_groups.detect { |ka| ka.include?(key) }
+              canonical[pref] = [canonize_header_value(value)].flatten
+            end
+
+            canonical
+          end
+
+          # Attempts to canonize value from headers by looking for hidden multi-value lines usually
+          # separated by commas.
+          #
+          # @param value [Object] value to canonize
+          # @return [Object] original or canonized value
+          def canonize_header_value(value)
+            value = value.first if value.is_a?(Array) && value.count == 1 # ary with 1 could be hidden multi-val
+            value = value.split(',').map(&:strip) if value.is_a?(String)  # multi-vals can hide as ','-separated String
+            value
+          end
+
+          # Unifies canonized headers by transforming them into a body-like
+          # form. Output of this method should be directly parsable as
+          # `text/plain` found in body.
+          #
+          # @param headers [Hash] hash with canonized header key-value pairs
+          # @return [Array] an array of body-like lines
+          def unify_headers(headers)
+            lines = []
+            headers.each_pair do |k, v|
+              lines << v.map { |val| "#{k}: #{val}" }
+            end
+            lines.flatten.sort
+          end
+
+          # Checks for use of multiple keys from a single key name groups. See `key_name_groups` for a list
+          # of available groups. Mixed key notations will produce an error.
+          #
+          # @param headers_keys [Array] list of keys to check
+          # @raise [Occi::Core::Errors::ParsingError] if mixed key notations detected
+          def validate_header_keys!(headers_keys)
+            return unless key_name_groups.any? { |elm| (headers_keys & elm).count > 1 }
+
+            raise Occi::Core::Errors::ParsingError,
+                  "Headers #{headers_keys.inspect} contain mixed key notations"
+          end
+
+          # Returns a list of available key name groups accessible as constants by name on this class.
+          #
+          # @return [Array] list of available key name groups
+          def key_name_groups
+            KEY_NAMES.map { |kn| const_get kn }
           end
         end
 
