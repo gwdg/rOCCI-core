98d93066e4230d8e25257f6b5c4e18aaeba63d05
Boris Parak <parak@cesnet.cz>
2017-05-01 13:22:57 +0200

    [JP] Added JSON-schema for OCCI (Draft v0.4)

---
+++ lib/occi/core/parsers/json/validator.rb
@@ -0,0 +1,13 @@
+module Occi
+  module Core
+    module Parsers
+      module Json
+        # Module responsible for validating JSON content before parsing. This should be included in
+        # every parsing class.
+        #
+        # @author Boris Parak <parak@cesnet.cz>
+        module Validator; end
+      end
+    end
+  end
+end
